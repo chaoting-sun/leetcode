1a45d0cc23a46a33d2e871714890db0a826d03d3
Sun, Chao-Ting <105558316+chaoting-sun@users.noreply.github.com>
2023-09-22 21:17:46 +0800

    Time: 104 ms (57.24%), Space: 24.9 MB (11.04%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,3 +1,22 @@
+### Brute force
+
+# class Solution(object):
+#     def twoSum(self, nums, target):
+#         """
+#         :type nums: List[int]
+#         :type target: int
+#         :rtype: List[int]
+#         """
+#         for i in range(len(nums)-1):
+#             for j in range(i+1, len(nums)):
+#                 if nums[i] + nums[j] == target:
+#                     return [i, j]
+#         return []
+
+
+
+import numpy as np
+
 class Solution(object):
     def twoSum(self, nums, target):
         """
@@ -5,10 +24,18 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        for i in range(len(nums)-1):
-            for j in range(i+1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
+        i = 0
+        j = len(nums) - 1
+
+        nums = np.array(nums)
+        ids = np.argsort(nums)
+        nums = nums[ids] # sort
+
+        while i < j:
+            if nums[i] + nums[j] == target:
+                return [ids[i], ids[j]]
+            elif nums[i] + nums[j] < target:
+                i += 1
+            elif nums[i] + nums[j] > target:
+                j -= 1
         return []
-            
-        
