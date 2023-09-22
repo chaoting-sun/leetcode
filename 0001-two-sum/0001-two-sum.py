f4f6f6556ee47001c200b171cdadf4f68ed68a3d
Sun, Chao-Ting <105558316+chaoting-sun@users.noreply.github.com>
2023-09-22 21:29:28 +0800

    Time: 42 ms (67.17%), Space: 13.9 MB (94.71%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,4 +1,4 @@
-### Brute force
+### method1: Brute force
 
 # class Solution(object):
 #     def twoSum(self, nums, target):
@@ -14,8 +14,35 @@
 #         return []
 
 
+### method2: argsort
 
-import numpy as np
+# import numpy as np
+
+# class Solution(object):
+#     def twoSum(self, nums, target):
+#         """
+#         :type nums: List[int]
+#         :type target: int
+#         :rtype: List[int]
+#         """
+#         i = 0
+#         j = len(nums) - 1
+
+#         nums = np.array(nums)
+#         ids = np.argsort(nums)
+#         nums = nums[ids] # sort
+
+#         while i < j:
+#             if nums[i] + nums[j] == target:
+#                 return [ids[i], ids[j]]
+#             elif nums[i] + nums[j] < target:
+#                 i += 1
+#             elif nums[i] + nums[j] > target:
+#                 j -= 1
+#         return []
+
+
+### method3: hash table
 
 class Solution(object):
     def twoSum(self, nums, target):
@@ -24,18 +51,9 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        i = 0
-        j = len(nums) - 1
-
-        nums = np.array(nums)
-        ids = np.argsort(nums)
-        nums = nums[ids] # sort
-
-        while i < j:
-            if nums[i] + nums[j] == target:
-                return [ids[i], ids[j]]
-            elif nums[i] + nums[j] < target:
-                i += 1
-            elif nums[i] + nums[j] > target:
-                j -= 1
+        hash_table = { nums[i]: i for i in range(len(nums)) }
+        for i in range(len(nums)):
+            left = target - nums[i]
+            if left in hash_table and hash_table[left] != i:
+                return [i, hash_table[left]]
         return []
