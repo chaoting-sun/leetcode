9ca7b4f35e22fe50bea245e66fad262cb17fe4ec
Sun, Chao-Ting <105558316+chaoting-sun@users.noreply.github.com>
2023-09-23 16:46:07 +0800

    Time: 39 ms (52.68%), Space: 14 MB (52.54%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,22 +1,42 @@
+# method1: hashmap
+
+# class Solution(object):
+#     def lengthOfLongestSubstring(self, s):
+#         """
+#         :type s: str
+#         :rtype: int
+#         """
+#         chars = {}
+#         max_len = 0
+#         left = 0
+
+#         for right in range(len(s)):
+#             if s[right] in chars:
+#                 max_len = max(max_len, right-left)
+#                 if chars[s[right]] >= left:
+#                     left = chars[s[right]] + 1
+#             chars[s[right]] = right
+
+#         return max(max_len, len(s)-left)
+
+
 class Solution(object):
     def lengthOfLongestSubstring(self, s):
         """
         :type s: str
         :rtype: int
         """
-        chars = {}
+        left = right = 0
+        chars = set()
         max_len = 0
-        sid = 0
 
-        for eid in range(len(s)):
-            if s[eid] in chars:
-                max_len = max(max_len, eid-sid)
-                if chars[s[eid]] >= sid:
-                    sid = chars[s[eid]] + 1
-                chars[s[eid]] = eid
+        for right in range(len(s)):
+            if s[right] not in chars:
+                chars.add(s[right])
+                max_len = max(max_len, right - left + 1)
             else:
-                chars[s[eid]] = eid
-
-        return max(max_len, len(s)-sid)
-
-            
+                while s[right] in chars:
+                    chars.remove(s[left])
+                    left += 1
+                chars.add(s[right])
+        return max_len
