8db04c2c5c76ce649c34d9b03152185488e98946
Sun, Chao-Ting <105558316+chaoting-sun@users.noreply.github.com>
2023-09-23 22:56:18 +0800

    Time: 333 ms (90.19%), Space: 13.4 MB (81.02%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -50,7 +50,7 @@
 #         return ans
 
 
-# method2: expand around center
+### method2: expand around center (EAC)
 
 class Solution(object):
     def substr(self, s, l, r):
@@ -71,15 +71,76 @@ class Solution(object):
         max_len = 0
 
         for i in range(len(s)-1):
+            # odd
             odd_l, odd_r = self.substr(s, i, i)
-            even_l, even_r = self.substr(s, i, i+1)
-
             if max_len < odd_r - odd_l - 1:
                 max_len = odd_r - odd_l - 1
                 ans = s[odd_l+1:odd_r]
 
+            # even
+            even_l, even_r = self.substr(s, i, i+1)
             if max_len < even_r - even_l - 1:
                 max_len = even_r - even_l - 1
                 ans = s[even_l+1:even_r]
         
         return ans
+
+
+### method3: dynamic programming (TLE)
+
+# import numpy as np
+
+# class Solution(object):
+#     def longestPalindrome(self, s):
+#         """
+#         :type s: str
+#         :rtype: str
+#         """
+#         ans = s[0]
+#         max_len = 1
+#         dp = np.ones((len(s), len(s))) # default: True
+        
+#         for j in range(len(s)):
+#             for i in range(j):
+#                 if i + 1 == j and s[i] != s[j]:
+#                     dp[i][j] = 0
+
+#                 if dp[i+1][j-1] == 0 or s[i] != s[j]:
+#                     dp[i][j] = 0
+
+#                 if dp[i][j] == 1 and j - i + 1 > max_len:
+#                     max_len = j - i + 1
+#                     ans = s[i:j+1]            
+#         return ans
+
+
+# method4: dynamic programming (TLE)
+
+# import numpy as np
+
+# class Solution(object):
+#     def longestPalindrome(self, s):
+#         """
+#         :type s: str
+#         :rtype: str
+#         """
+#         ans = s[0]
+#         max_len = 1
+#         dp = np.ones((len(s), len(s))) # default: True
+        
+#         for j in range(len(s)):
+#             for i in range(j):
+#                 if i + 1 == j and s[i] != s[j]:
+#                     dp[i][j] = 0
+
+#                 if dp[i+1][j-1] == 0 or s[i] != s[j]:
+#                     dp[i][j] = 0
+
+#                 if dp[i][j] == 1 and j - i + 1 > max_len:
+#                     max_len = j - i + 1
+#                     ans = s[i:j+1]            
+#         return ans
+
+
+### method5: Manacher algorithm (Time complexity = O(N))
+# too hard
