6ceb53540bcd04262b09c2e3ebccabbd1df0f943
Sun, Chao-Ting <105558316+chaoting-sun@users.noreply.github.com>
2023-09-24 22:15:30 +0800

    Time: 18 ms (61.55%), Space: 13.3 MB (88.09%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
@@ -1,4 +1,63 @@
-import numpy as np
+### method1: mine
+
+# import numpy as np
+
+# class Solution(object):
+#     def myAtoi(self, s):
+#         """
+#         :type s: str
+#         :rtype: int
+#         """
+#         valid_char = ['0', '1', '2', '3', '4',
+#                       '5', '6', '7', '8', '9']
+
+#         sign = 0
+#         p = 0
+
+#         while p < len(s):
+#             if s[p] == ' ':
+#                 if sign != 0:
+#                     return 0
+#                 p += 1
+#                 continue
+#             elif s[p] == '+' or s[p] == '-':
+#                 if sign == 0:
+#                     sign = 1 if s[p] == '+' else -1
+#                 else:
+#                     return 0
+#                 p += 1
+#                 continue
+#             elif s[p] in valid_char:
+#                 break
+#             else: # other character
+#                 return 0
+
+#         digits = []
+
+#         for i in range(p, len(s)):
+#             try:
+#                 digits.append(int(s[i]))
+#             except:
+#                 break
+
+#         ans = 0
+#         ub = 2**31-1
+#         lb = -2**31
+
+#         for i in range(len(digits)):
+#             ans += digits[i] * 10**(len(digits)-i-1)
+
+#         ans = sign*ans if sign == -1 else ans
+
+#         if ans > ub:
+#             return ub
+#         elif ans < lb:
+#             return lb
+#         else:
+#             return ans
+
+
+### method2: faster
 
 class Solution(object):
     def myAtoi(self, s):
@@ -6,59 +65,44 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        valid_char = ['0', '1', '2', '3', '4',
-                      '5', '6', '7', '8', '9']
+        p = 0
+        n = len(s)
 
-        sign = 0
-        ptr = 0
+        while p < n and s[p] == ' ':
+            p += 1
+        
+        positive = 0
+        negative = 0
 
-        while ptr < len(s):
-            print(ptr, s[ptr])
+        if p < n and s[p] == '+':
+            positive = 1
+            p += 1
+        
+        if p < n and s[p] == '-':
+            negative = 1
+            p += 1
+        
+        ans = 0
 
-            if s[ptr] == ' ':
-                if sign != 0:
-                    return 0
-                ptr += 1
-                continue
-            elif s[ptr] == '+' or s[ptr] == '-':
-                if sign == 0:
-                    sign = 1 if s[ptr] == '+' else -1
-                else:
-                    return 0
-                ptr += 1
-                continue
-            elif s[ptr] in valid_char:
-                break
-            else: # other character
-                return 0
+        while p < n and '0' <= s[p] <= '9':
+            ans = ans * 10 + (ord(s[p]) - ord('0'))
+            p += 1
 
-        digits = []
+        if negative == 1:
+            ans *= -1
 
-        for i in range(ptr, len(s)):
-            try:
-                digits.append(int(s[i]))
-            except:
-                break
+        if positive == 1 and negative == 1:
+            return 0
 
-        mysum = 0
-        ub = 2**31-1
-        lb = -2**31
+        ub = 2**31 - 1
+        lb = - 2**31
 
-        for i in range(len(digits)):
-            mysum += digits[i] * 10**(len(digits)-i-1)
-
-        mysum = sign*mysum if sign == -1 else mysum
-
-        if mysum > ub:
+        if ans > ub:
             return ub
-        elif mysum < lb:
+        elif ans < lb:
             return lb
         else:
-            return mysum
-
-
-        
-
+            return ans          
             
 
 
