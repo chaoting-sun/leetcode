1e563b9b30eae0450b727d46e8922ec16d59423c
Sun, Chao-Ting <105558316+chaoting-sun@users.noreply.github.com>
2023-09-25 15:17:17 +0800

    Time: 541 ms (25.2%), Space: 23.6 MB (98.15%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -0,0 +1,37 @@
+# method1: brute force (TLE)
+
+# class Solution(object):
+#     def maxArea(self, height):
+#         """
+#         :type height: List[int]
+#         :rtype: int
+#         """
+#         maxArea = 0
+
+#         for i in range(len(height)-1):
+#             for j in range(i+1, len(height)):
+#                 area = (j-i) * min(height[i], height[j])
+#                 maxArea = max(maxArea, area)
+#         return maxArea
+
+
+class Solution(object):
+    def maxArea(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        l = 0
+        r = len(height) - 1
+        maxArea = 0
+        
+        while l < r:
+            currArea = min(height[l], height[r]) * (r - l)
+            maxArea = max(maxArea, currArea)
+            
+            if height[l] < height[r]:
+                l += 1
+            else:
+                r -= 1
+        return maxArea
+        
