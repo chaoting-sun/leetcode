08755056405e2b7d612adfaa90dfbefcf8b38b5b
Sun, Chao-Ting <105558316+chaoting-sun@users.noreply.github.com>
2023-09-26 13:09:00 +0800

    Time: 6 ms (97.84%), Space: 13.3 MB (53.46%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,3 +1,53 @@
+### method1: fill characters in order (mine)
+
+# class Solution(object):
+#     def letterCombinations(self, digits):
+#         """
+#         :type digits: str
+#         :rtype: List[str]
+#         """
+#         if len(digits) == 0:
+#             return []
+
+#         d2e = {
+#             '2': ['a', 'b', 'c'],
+#             '3': ['d', 'e', 'f'],
+#             '4': ['g', 'h', 'i'],
+#             '5': ['j', 'k', 'l'],
+#             '6': ['m', 'n', 'o'],
+#             '7': ['p', 'q', 'r', 's'],
+#             '8': ['t', 'u', 'v'],
+#             '9': ['w', 'x', 'y', 'z']
+#         }
+
+#         combs = [d2e[d] for d in digits]
+        
+#         n_total_comb = 1
+#         for c in combs:
+#             n_total_comb *= len(c)
+        
+#         n_comb_each = []
+#         n_curr_comb = n_total_comb
+#         for c in combs:
+#             n_curr_comb /= len(c)
+#             n_comb_each.append(n_curr_comb)
+
+#         ans = ['' for i in range(n_total_comb)]
+        
+#         for i in range(len(combs)):
+#             s = ''
+#             for j in range(len(combs[i])):
+#                 s += combs[i][j] * n_comb_each[i]
+#             total_s = s * (n_total_comb / len(s))
+
+#             for k, s in enumerate(total_s):
+#                 ans[k] += s
+
+#         return ans
+
+
+### method2: BFS
+
 class Solution(object):
     def letterCombinations(self, digits):
         """
@@ -7,38 +57,14 @@ class Solution(object):
         if len(digits) == 0:
             return []
 
-        d2e = {
-            '2': ['a', 'b', 'c'],
-            '3': ['d', 'e', 'f'],
-            '4': ['g', 'h', 'i'],
-            '5': ['j', 'k', 'l'],
-            '6': ['m', 'n', 'o'],
-            '7': ['p', 'q', 'r', 's'],
-            '8': ['t', 'u', 'v'],
-            '9': ['w', 'x', 'y', 'z']
-        }
+        d2e = ['', '', 'abc', 'def', 'ghi', 'jkl',
+               'mno', 'pqrs', 'tuv', 'wxyz']
+        ans = ['']
 
-        combs = [d2e[d] for d in digits]
-        
-        n_total_comb = 1
-        for c in combs:
-            n_total_comb *= len(c)
-        
-        n_comb_each = []
-        n_curr_comb = n_total_comb
-        for c in combs:
-            n_curr_comb /= len(c)
-            n_comb_each.append(n_curr_comb)
-
-        ans = ['' for i in range(n_total_comb)]
-        
-        for i in range(len(combs)):
-            s = ''
-            for j in range(len(combs[i])):
-                s += combs[i][j] * n_comb_each[i]
-            total_s = s * (n_total_comb / len(s))
-
-            for k, s in enumerate(total_s):
-                ans[k] += s
-
+        for d in digits:
+            tmp = []
+            for a in ans:
+                for e in d2e[ord(d)-ord('0')]:
+                    tmp.append(a + e)
+            ans = tmp
         return ans
