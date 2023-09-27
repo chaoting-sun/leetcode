97292cbd376eeabb86f117a6727ba01243c9bf51
Sun, Chao-Ting <105558316+chaoting-sun@users.noreply.github.com>
2023-09-28 01:13:42 +0800

    Time: 72 ms (72.68%), Space: 19.2 MB (91.94%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -80,4 +80,51 @@ class Solution(object):
         tail.next = None
         return dummyNode.next
 
+
+### method3: merge sort
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def merge2Lists(self, l1, l2):
+        dummyNode = ListNode(0, None)
+        tail = dummyNode
+        p1, p2 = l1, l2
         
+        while p1 and p2:
+            if p1.val < p2.val:
+                tail.next = p1
+                p1 = p1.next
+            else:
+                tail.next = p2
+                p2 = p2.next
+            tail = tail.next
+        if p1: tail.next = p1
+        if p2: tail.next = p2
+
+        return dummyNode.next
+
+    def merge(self, lists, l, r):
+        if l > r:
+            return None
+        if l == r:
+            return lists[l]
+        elif l + 1 == r:
+            return self.merge2Lists(lists[l], lists[r])
+        
+        c = l + (r-l)//2
+
+        left = self.merge(lists, l, c)
+        right = self.merge(lists, c+1, r)
+        return self.merge2Lists(left, right)
+
+    def mergeKLists(self, lists):
+        """
+        :type lists: List[ListNode]
+        :rtype: ListNode
+        """
+        return self.merge(lists, 0, len(lists)-1)
+
