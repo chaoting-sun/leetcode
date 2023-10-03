b089585cf0d9bc168304a7ed8b3536a69d2bfa94
Sun, Chao-Ting <105558316+chaoting-sun@users.noreply.github.com>
2023-10-03 07:43:06 -0500

    Time: 25 ms (41.56%), Space: 13.5 MB (42.52%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -0,0 +1,71 @@
+# method1
+
+# class Solution(object):
+#     def binarySearch(self, nums, l, r, target):
+#         while l <= r:
+#             c = (l + r) // 2
+#             if nums[c] == target:
+#                 return c
+#             elif nums[c] > target:
+#                 r = c - 1
+#             elif nums[c] < target:
+#                 l = c + 1
+#         return -1
+
+#     def search(self, nums, target):
+#         """
+#         :type nums: List[int]
+#         :type target: int
+#         :rtype: int
+#         """
+#         n = len(nums)
+
+#         l, r = 0, n - 1
+
+#         while l < r:
+#             c = (l + r) // 2
+#             if nums[c] < nums[r]:
+#                 r = c
+#             else:
+#                 l = c + 1
+#         pivot = l
+
+#         if pivot == 0:
+#             return self.binarySearch(nums, 0, n-1, target)
+#         else:
+#             if nums[0] <= target <= nums[pivot-1]:
+#                 return self.binarySearch(nums, 0, pivot-1, target)
+#             else:
+#                 return self.binarySearch(nums, pivot, n-1, target)
+
+
+# method2 (hard to understand)
+
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        n = len(nums)
+
+        l, r = 0, n - 1
+
+        while l <= r:
+            c = (l + r) // 2
+
+            if nums[c] == target:
+                return c
+            
+            if nums[l] <= nums[c]:
+                if nums[l] <= target < nums[c]:
+                    r = c - 1
+                else:
+                    l = c + 1
+            else:
+                if nums[c] < target <= nums[r]:
+                    l = c + 1
+                else:
+                    r = c - 1
+        return -1
