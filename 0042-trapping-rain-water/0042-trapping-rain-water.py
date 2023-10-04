6e16366b2e32ed2444746807dfcec9f9d1174366
Sun, Chao-Ting <105558316+chaoting-sun@users.noreply.github.com>
2023-10-04 02:50:55 -0500

    Time: 86 ms (49.82%), Space: 14.8 MB (67.36%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,27 +1,4 @@
-class Solution(object):
-    def trap(self, height):
-        """
-        :type height: List[int]
-        :rtype: int
-        """
-        n = len(height)
-        prefixMax = [0] * n
-        suffixMax = [0] * n
-        
-        maxV = 0
-        for i in range(n):
-            maxV = max(maxV, height[i])
-            prefixMax[i] = maxV
-        maxV = 0
-        for i in range(n-1, -1, -1):
-            maxV = max(maxV, height[i])
-            suffixMax[i] = maxV
-
-        vol = 0
-        for h, pref, suff in zip(height, prefixMax, suffixMax):
-            vol += min(pref, suff) - h
-        return vol
-
+# method1: dp
 
 # class Solution(object):
 #     def trap(self, height):
@@ -29,35 +6,48 @@ class Solution(object):
 #         :type height: List[int]
 #         :rtype: int
 #         """
-#         vol = 0
 #         n = len(height)
+#         prefixMax = [0] * n
+#         suffixMax = [0] * n
+        
+#         maxV = 0
+#         for i in range(n):
+#             maxV = max(maxV, height[i])
+#             prefixMax[i] = maxV
+#         maxV = 0
+#         for i in range(n-1, -1, -1):
+#             maxV = max(maxV, height[i])
+#             suffixMax[i] = maxV
 
-#         if n == 1 or n == 2:
-#             return 0
-
-#         l = mid = r = 1
-
-#         while mid < n - 1:
-#             hmax = height[mid]
-#             while l > 0 and height[l-1] > height[l]:
-#                 l -= 1
-#             while r < n - 1 and height[r] < height[r+1]:
-#                 r += 1
-#             hmax = max(hmax, height[l], height[r])
-
-#             print(l, mid, r, hmax)
-
-#             if hmax == height[mid]:
-#                 mid += 1
-#                 l = r = mid
-#             else:
-#                 hmin = min(height[l], height[r])
-#                 print('hmin:', hmin)
-#                 for i in range(l+1, r):
-#                     vol += (hmin - height[i])
-#                 l = r = mid = r + 1
-#             print(vol)
+#         vol = 0
+#         for h, pref, suff in zip(height, prefixMax, suffixMax):
+#             vol += min(pref, suff) - h
 #         return vol
+
+
+# method2: two pointers
+
+class Solution(object):
+    def trap(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        left, right = 0, len(height) - 1
+        leftMax = rightMax = 0
+        vol = 0
+
+        while left < right:
+            leftMax = max(leftMax, height[left])
+            rightMax = max(rightMax, height[right])
+
+            if leftMax > rightMax:
+                vol += rightMax - height[right]
+                right -= 1
+            else:
+                vol += leftMax - height[left]
+                left += 1
+        return vol
 
 
             
