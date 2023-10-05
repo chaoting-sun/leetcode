50d2b8b30b516039d7ac3220d511eeab9c741660
Sun, Chao-Ting <105558316+chaoting-sun@users.noreply.github.com>
2023-10-05 05:11:58 -0500

    Time: 13 ms (64.26%), Space: 13.1 MB (95.67%) - LeetHub

---
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -0,0 +1,60 @@
+class Solution(object):
+    def spiralOrder(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: List[int]
+        """
+
+        nRows, nCols = len(matrix), len(matrix[0])
+        nEle = nRows * nCols
+        upRight, downRight, downLeft, upLeft = nCols, nRows, -1, -1
+
+        ans = [matrix[0][0]]
+        i = 0
+        j = 1
+        print(matrix)
+
+        while True:
+            while j < upRight:
+                ans.append(matrix[i][j])
+                j += 1
+            j -= 1
+            
+            upLeft += 1
+            i += 1
+
+            if len(ans) == nEle:
+                break
+
+            while i < downRight:
+                ans.append(matrix[i][j])
+                i += 1
+            i -= 1
+
+            upRight -= 1
+            j -= 1
+
+            if len(ans) == nEle:
+                break
+
+            while j > downLeft:
+                ans.append(matrix[i][j])
+                j -= 1
+            j += 1
+            downRight -= 1
+            i -= 1
+
+            if len(ans) == nEle:
+                break
+
+            while i > upLeft:
+                ans.append(matrix[i][j])
+                i -= 1
+            i += 1
+
+            downLeft += 1
+            j += 1
+
+            if len(ans) == nEle:
+                break
+        return ans
