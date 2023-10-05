e7184dda2bbd857b1078142e287cdb72dfb2cf82
Sun, Chao-Ting <105558316+chaoting-sun@users.noreply.github.com>
2023-10-05 03:00:47 -0500

    Time: 531 ms (62.3%), Space: 26 MB (89.92%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -0,0 +1,88 @@
+### method1: dp (O(n^2); TLE)
+
+# import numpy as np
+
+# class Solution(object):
+#     def maxSubArray(self, nums):
+#         """
+#         :type nums: List[int]
+#         :rtype: int
+#         """
+#         n = len(nums)
+#         dp = np.zeros((n,n), dtype=int)
+#         maxSum = -10E4
+
+#         for j in range(n):
+#             for i in range(j+1):
+#                 if i == j:
+#                     dp[i][j] = nums[i]
+#                 else:
+#                     dp[i][j] = dp[i][j-1] + nums[j]
+#                 maxSum = max(maxSum, dp[i][j])
+#         return maxSum
+
+
+### method2: Kadane's algorithm (O(n))
+
+# import numpy as np
+
+# class Solution(object):
+#     def maxSubArray(self, nums):
+#         """
+#         :type nums: List[int]
+#         :rtype: int
+#         """
+#         n = len(nums)
+
+#         maxSum = currMaxSum = nums[0]
+        
+#         for i in range(1, n):
+#             if currMaxSum > 0:
+#                 currMaxSum += nums[i]
+#             else:
+#                 currMaxSum = nums[i]
+#             maxSum = max(maxSum, currMaxSum)
+#         return maxSum
+
+
+### method3: prefix sum (O(n))
+
+# class Solution(object):
+#     def maxSubArray(self, nums):
+#         """
+#         :type nums: List[int]
+#         :rtype: int
+#         """
+#         n = len(nums)
+
+#         prefixSum = [0] * (n+1)
+#         prefixSum[0] = 0
+#         for i in range(n):
+#             prefixSum[i+1] = prefixSum[i] + nums[i]
+#         minSum = float('inf')
+#         maxSum = -float('-inf')
+
+#         for i in range(1, n+1):
+#             minSum = min(minSum, prefixSum[i-1])
+#             maxSum = max(maxSum, prefixSum[i] - minSum)
+#         return maxSum
+
+
+### method3.1 prefix sum (faster)
+
+class Solution(object):
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        minSum = 0
+        maxSum = float('-inf')
+        prefixSum = 0
+
+        for k in nums:
+            prefixSum += k
+            maxSum = max(maxSum, prefixSum-minSum)
+            minSum = min(minSum, prefixSum)
+        return maxSum
+
