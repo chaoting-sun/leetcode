add4210ef791388c5c52dfe7b6c291434979e19d
Sun, Chao-Ting <105558316+chaoting-sun@users.noreply.github.com>
2023-10-06 05:37:57 -0500

    Time: 30 ms (7.31%), Space: 13.3 MB (87.36%) - LeetHub

--- 0067-add-binary/0067-add-binary.py
+++ 0067-add-binary/0067-add-binary.py
@@ -1,3 +1,49 @@
+### mine
+
+# class Solution(object):
+#     def addBinary(self, a, b):
+#         """
+#         :type a: str
+#         :type b: str
+#         :rtype: str
+#         """
+#         ans = ''
+#         i, j = len(a)-1, len(b)-1
+#         tmp = 0
+
+#         while i >= 0 or j >= 0:
+#             print(i, j)
+#             if i < 0:
+#                tmp += int(b[j])
+#                j -= 1
+#             elif j < 0:
+#                 tmp += int(a[i])
+#                 i -= 1
+#             else:
+#                 tmp += int(a[i]) + int(b[j])
+#                 i -= 1
+#                 j -= 1
+            
+#             if tmp == 0:
+#                 ans = '0' + ans
+#             elif tmp == 1:
+#                 ans = '1' + ans
+#                 tmp = 0
+#             elif tmp == 2:
+#                 ans = '0' + ans
+#                 tmp = 1
+#             elif tmp == 3:
+#                 ans = '1' + ans
+#                 tmp = 1
+
+#         if tmp == 1:
+#             return '1' + ans
+#         else:
+#             return ans
+
+
+### cleaner
+
 class Solution(object):
     def addBinary(self, a, b):
         """
@@ -7,34 +53,15 @@ class Solution(object):
         """
         ans = ''
         i, j = len(a)-1, len(b)-1
-        tmp = 0
+        carry = 0
 
-        while i >= 0 or j >= 0:
-            print(i, j)
-            if i < 0:
-               tmp += int(b[j])
-               j -= 1
-            elif j < 0:
-                tmp += int(a[i])
+        while i >= 0 or j >= 0 or carry:
+            if i >= 0:
+                carry += int(a[i])
                 i -= 1
-            else:
-                tmp += int(a[i]) + int(b[j])
-                i -= 1
+            if j >= 0:
+                carry += int(b[j])
                 j -= 1
-            
-            if tmp == 0:
-                ans = '0' + ans
-            elif tmp == 1:
-                ans = '1' + ans
-                tmp = 0
-            elif tmp == 2:
-                ans = '0' + ans
-                tmp = 1
-            elif tmp == 3:
-                ans = '1' + ans
-                tmp = 1
-
-        if tmp == 1:
-            return '1' + ans
-        else:
-            return ans
+            ans = str(carry % 2) + ans
+            carry //= 2
+        return ans
