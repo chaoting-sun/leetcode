129a2f02bc9b8c3ce4cb56ba8b0c7f49da571353
Sun, Chao-Ting <105558316+chaoting-sun@users.noreply.github.com>
2023-10-06 03:38:58 -0500

    Time: 49 ms (76.99%), Space: 16.6 MB (64.75%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -1,3 +1,48 @@
+### method1
+
+# class Solution(object):
+#     def insert(self, intervals, newInterval):
+#         """
+#         :type intervals: List[List[int]]
+#         :type newInterval: List[int]
+#         :rtype: List[List[int]]
+#         """
+#         n = len(intervals)
+
+#         if n == 0:
+#             return [newInterval]
+        
+#         head = 0
+#         tail = n - 1
+#         ans = []
+
+#         while head < n and newInterval[0] > intervals[head][1]:
+#             head += 1
+
+#         while tail >= 0 and newInterval[1] < intervals[tail][0]:
+#             tail -= 1
+
+#         # left
+#         if head > 0:
+#             ans = ans + intervals[:head]
+        
+#         # center
+#         if head == n:
+#             ans = ans + [newInterval]
+#         elif tail == -1:
+#             ans = [newInterval] + ans
+#         else:
+#             ans = ans + [[min(intervals[head][0], newInterval[0]), max(intervals[tail][1], newInterval[1])]]
+        
+#         # right
+#         if tail + 1 < n:
+#             ans = ans + intervals[tail+1:]
+
+#         return ans
+
+
+### method2
+
 class Solution(object):
     def insert(self, intervals, newInterval):
         """
@@ -5,33 +50,23 @@ class Solution(object):
         :type newInterval: List[int]
         :rtype: List[List[int]]
         """
-        n = len(intervals)
-
-        if n == 0:
-            return [newInterval]
-        
-        head = 0
-        tail = n - 1
         ans = []
 
-        while head < n and newInterval[0] > intervals[head][1]:
-            head += 1
+        i = 0
+        n = len(intervals)
 
-        while tail >= 0 and newInterval[1] < intervals[tail][0]:
-            tail -= 1
-        print(head, tail)
+        while i < n:
+            if intervals[i][1] < newInterval[0]:
+                ans.append(intervals[i])
+            elif intervals[i][0] > newInterval[1]:
+                break
+            else:
+                newInterval[0] = min(newInterval[0], intervals[i][0])
+                newInterval[1] = max(newInterval[1], intervals[i][1])
+            i += 1
+        ans.append(newInterval)
 
-        if head > 0:
-            ans = ans + intervals[:head]
-        
-        if head == n:
-            ans = ans + [newInterval]
-        elif tail == -1:
-            ans = [newInterval] + ans
-        else:
-            ans = ans + [[min(intervals[head][0], newInterval[0]), max(intervals[tail][1], newInterval[1])]]
-        
-        if tail + 1 < n:
-            ans = ans + intervals[tail+1:]
-
+        while i < n:
+            ans.append(intervals[i])
+            i += 1
         return ans
