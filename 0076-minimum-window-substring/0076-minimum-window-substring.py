a728c3bae3a3202d94b358108fe1265ee8f9080d
Sun, Chao-Ting <105558316+chaoting-sun@users.noreply.github.com>
2023-10-08 06:02:26 -0500

    Time: 96 ms (89.08%), Space: 17.2 MB (46.86%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,3 +1,5 @@
+# sliding window (hard)
+
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
         record = {}
@@ -10,22 +12,26 @@ class Solution:
         left, right = 0, 0
         minLeft, minLen = 0, 10E5
 
+        # 'right' starts from 0 and moves to find a valid window
         while right < size:
-
+            # decrease cnt if s[right] in record
             if s[right] in record and record[s[right]] > 0:
                 cnt -= 1
             
+            # decrease record[s[right]], including s[right] not in t
             if s[right] not in record:
                 record[s[right]] = 0
             record[s[right]] -= 1
 
             right += 1
 
+            # when we find a valid window, move right to find a smaller window s.t. cnt is still 0
             while cnt == 0:
                 if right - left < minLen:
                     minLeft = left
                     minLen = right - left
                 record[s[left]] += 1
+                # when s[left] in t and s[left] > 0 then increase cnt
                 if record[s[left]] > 0:
                     cnt += 1
                 left += 1                
@@ -33,3 +39,6 @@ class Solution:
         if minLen == 10E5:
             return ''
         return s[minLeft: minLeft+minLen]
+
+    
+# nice source: https://leetcode.com/problems/minimum-window-substring/solutions/26808/here-is-a-10-line-template-that-can-solve-most-substring-problems/?envType=list&envId=rab78cw1
