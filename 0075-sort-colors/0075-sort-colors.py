081657d27a5a22f4c6a5df316a18494695ba3b0a
Sun, Chao-Ting <105558316+chaoting-sun@users.noreply.github.com>
2023-10-08 03:33:10 -0500

    Time: 31 ms (97.17%), Space: 16.2 MB (56.01%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -1,16 +1,40 @@
+### method1: two-pass algorithm
+
+# class Solution:
+#     def sortColors(self, nums: List[int]) -> None:
+#         """
+#         Do not return anything, modify nums in-place instead.
+#         """
+#         rec = { 0: 0, 1: 0, 2: 0 }
+#         for i in range(len(nums)):
+#             rec[nums[i]] += 1
+
+#         for i in range(rec[0]):
+#             nums[i] = 0
+#         for i in range(rec[0], rec[0]+rec[1]):
+#             nums[i] = 1
+#             for i in range(rec[0]+rec[1], rec[0]+rec[1]+rec[2]):
+#                 nums[i] = 2
+#             return nums
+
+
+### method2: one-pass algorithm
+
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
-        rec = { 0: 0, 1: 0, 2: 0 }
-        for i in range(len(nums)):
-            rec[nums[i]] += 1
-
-        for i in range(rec[0]):
-            nums[i] = 0
-        for i in range(rec[0], rec[0]+rec[1]):
-            nums[i] = 1
-        for i in range(rec[0]+rec[1], rec[0]+rec[1]+rec[2]):
-            nums[i] = 2
+        low, mid, high = 0, 0, len(nums)-1
+        
+        while mid <= high:
+            if nums[mid] == 0:
+                nums[low], nums[mid] = nums[mid], nums[low]
+                low += 1
+                mid += 1
+            elif nums[mid] == 2:
+                nums[mid], nums[high] = nums[high], nums[mid]
+                high -= 1
+            else:
+                mid += 1
         return nums
