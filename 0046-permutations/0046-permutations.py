419afc019be17f40d02b64654825bedda22511d8
Sun, Chao-Ting <105558316+chaoting-sun@users.noreply.github.com>
2023-10-09 02:03:45 -0500

    Time: 44 ms (63.1%), Space: 16.4 MB (56.64%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,20 +1,21 @@
 ### method1: dfs
 
 class Solution:
-    def dfs(self, prevNums, maxL, prevSet, ans):
-        if len(prevSet) == maxL:
-            ans.append(prevSet)
+    def dfs(self, size, prevNums, subset, ans):
+        if len(subset) == size:
+            ans.append(subset)
             return
         
         for i in range(len(prevNums)):
-            curSet = prevSet + [prevNums[i]]
+            curSubset = subset + [prevNums[i]]
             curNums = prevNums[:i] + prevNums[i+1:]
-            self.dfs(curNums, maxL, curSet, ans)
-            
+            self.dfs(size, curNums, curSubset, ans)
+
     def permute(self, nums: List[int]) -> List[List[int]]:
         ans = []
-        self.dfs(nums, len(nums), [], ans)
+        self.dfs(len(nums), nums, [], ans)
         return ans
+
 
 ### nice source:
 # https://leetcode.com/problems/permutations/solutions/18239/a-general-approach-to-backtracking-questions-in-java-subsets-permutations-combination-sum-palindrome-partioning/?envType=list&envId=rab78cw1
