d6255e43ec97b2b2f147e12878598898cbaaee77
Sun, Chao-Ting <105558316+chaoting-sun@users.noreply.github.com>
2023-10-09 05:58:29 -0500

    Time: 814 ms (24.58%), Space: 30.7 MB (66.86%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
@@ -1,3 +1,7 @@
+### method1: stack (time complexity=O(N); space complexity=O(N))
+
+# https://leetcode.com/problems/largest-rectangle-in-histogram/solutions/28917/ac-python-clean-solution-using-stack-76ms/?envType=list&envId=rab78cw1
+
 class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
         ans = 0
@@ -13,4 +17,31 @@ class Solution:
         heights.pop()
         return ans
 
+
+### method2: dp (time complexity=?; space complexity=O(N))
+
+# https://leetcode.com/problems/largest-rectangle-in-histogram/solutions/28902/5ms-o-n-java-solution-explained-beats-96/?envType=list&envId=rab78cw1
+
+class Solution:
+    def largestRectangleArea(self, heights: List[int]) -> int:
+        n = len(heights)
+        lessFromLeft = [-1] * n
+        lessFromRight = [n] * n
+
         
+        for i in range(1, n):
+            p = i - 1
+            while p >= 0 and heights[p] >= heights[i]:
+                p = lessFromLeft[p]
+            lessFromLeft[i] = p
+        
+        for i in range(n-2, -1, -1):
+            p = i + 1
+            while p < n and heights[i] <= heights[p]:
+                p = lessFromRight[p]
+            lessFromRight[i] = p
+
+        ans = 0
+        for i in range(n):
+            ans = max(ans, heights[i] * (lessFromRight[i]-lessFromLeft[i]-1))
+        return ans
