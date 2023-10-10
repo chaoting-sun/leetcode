383cc08993aae7ae96d9d85637f92b78e0401f7e
Sun, Chao-Ting <105558316+chaoting-sun@users.noreply.github.com>
2023-10-10 05:33:06 -0500

    Time: 45 ms (91.83%), Space: 21 MB (63.93%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.py
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.py
@@ -4,15 +4,45 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    def isBalanced(self, root: Optional[TreeNode]) -> bool:
-        if not root: return True
 
-        def depth(node):
-            if not node: return 0
-            else: return max(depth(node.left), depth(node.right)) + 1
+### method1: top down approach
 
-        return abs(depth(root.left)-depth(root.right)) <= 1 \
-           and self.isBalanced(root.left) and self.isBalanced(root.right)
+# time complexity: O(N^2)
+# space complexity: O(N)
+
+# class Solution:
+#     def isBalanced(self, root: Optional[TreeNode]) -> bool:
+#         if not root: return True
+
+#         def depth(node):
+#             if not node: return 0
+#             else: return max(depth(node.left), depth(node.right)) + 1
+
+#         # use the definition of balanced binary tree:
+#         # the difference between the depths of the two sub trees
+#         # are not bigger than 1, and both of the left sub tree
+#         # and right sub tree are also balanced
+#         return abs(depth(root.left)-depth(root.right)) <= 1 \
+#            and self.isBalanced(root.left) and self.isBalanced(root.right)
             
-        
+
+### method2: down top approach; dfs
+
+class Solution:    
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        def dfsHeight(root):
+            if not root: return 0
+
+            # check if left sub tree is balanced
+            leftHeight = dfsHeight(root.left)
+            if leftHeight == -1: return -1
+
+            # check if right sub tree is balanced
+            rightHeight = dfsHeight(root.right)
+            if rightHeight == -1: return -1
+
+            # check if current tree is balance
+            if abs(leftHeight - rightHeight) > 1: return -1
+            else: return max(leftHeight, rightHeight) + 1
+
+        return dfsHeight(root) != -1
