196c2b031bcd046acdf5570223342c84dc6e7785
Sun, Chao-Ting <105558316+chaoting-sun@users.noreply.github.com>
2023-10-09 21:39:35 -0500

    Time: 47 ms (66.1%), Space: 18.6 MB (82.37%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -4,19 +4,56 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+
+### method1: dfs; preorder traversal
+
+# class Solution:
+#     def dfs(self, root, minBound, maxBound):
+#         if not root:
+#             return True
+#         if not minBound < root.val < maxBound:
+#             return False
+#         return self.recursion(root.left, minBound, root.val) \
+#            and self.recursion(root.right, root.val, maxBound)
+
+#     def isValidBST(self, root: Optional[TreeNode]) -> bool:
+#         return self.recursion(root, float('-inf'), float('inf'))
+
+
+### method2: inorder traversal with array
+
+# class Solution:
+#     def isValidBST(self, root: Optional[TreeNode]) -> bool:
+#         def inorderTraversal(root, arr):
+#             if root:
+#                 inorderTraversal(root.left, arr)
+#                 arr.append(root.val)
+#                 inorderTraversal(root.right, arr)
+#             return arr
+#         res = inorderTraversal(root, [])
+#         for i in range(1, len(res)):
+#             if res[i-1] >= res[i]:
+#                 return False
+#         return True
+
+
+### method3: inorder traversal with stack
+
 class Solution:
-    def recursion(self, root, leftMost, rightMost):
-        # termination
-        if not root:
-            return True
-
-        if root.left and not leftMost < root.left.val < root.val:
-            return False
-        if root.right and not root.val < root.right.val < rightMost:
-            return False
-
-        return self.recursion(root.left, leftMost, root.val) \
-           and self.recursion(root.right, root.val, rightMost)
-
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        return self.recursion(root, -2**32, 2**32)
+        stack = []
+        minBound = float('-inf')
+
+        while root or len(stack):
+            if root:
+                stack.append(root)
+                root = root.left
+            else:
+                node = stack.pop()
+                if minBound >= node.val:
+                    return False
+                else:
+                    minBound = node.val
+                    root = node.right
+        return True
