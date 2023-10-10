5e971564e124ab8b6900de397ddaf2d22b518813
Sun, Chao-Ting <105558316+chaoting-sun@users.noreply.github.com>
2023-10-09 23:50:51 -0500

    Time: 45 ms (55.92%), Space: 17 MB (79.63%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
@@ -4,27 +4,56 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    def preorderTraversal(self, root, level, arr):
-        if root:
-            arr.append((root.val, level))
-            self.preorderTraversal(root.left, level+1, arr)
-            self.preorderTraversal(root.right, level+1, arr)
 
-    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        arr = []
-        self.preorderTraversal(root, 0, arr)
-        arr.sort(key=lambda x: x[1])
 
-        ans = []
-        l = 0
-        i = 0
-        while i < len(arr):
-            tmp = []
-            while i < len(arr) and arr[i][1] == l:
-                tmp.append(arr[i][0])
-                i += 1
-            ans.append(tmp)
-            l += 1
-        return ans
+### method1: preorder traversal + additional array
+
+# class Solution:
+#     def preorderTraversal(self, root, level, arr):
+#         if root:
+#             arr.append((root.val, level))
+#             self.preorderTraversal(root.left, level+1, arr)
+#             self.preorderTraversal(root.right, level+1, arr)
+
+#     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+#         arr = []
+#         self.preorderTraversal(root, 0, arr)
+#         arr.sort(key=lambda x: x[1])
+
+#         ans = []
+#         l = 0
+#         i = 0
+#         while i < len(arr):
+#             tmp = []
+#             while i < len(arr) and arr[i][1] == l:
+#                 tmp.append(arr[i][0])
+#                 i += 1
+#             ans.append(tmp)
+#             l += 1
+#         return ans
             
+
+### method2: queue
+
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return []
+        
+        ans = []
+        queue = [root]
+
+        while len(queue):
+            size = len(queue)
+
+            tmp = []
+            while size:
+                root = queue.pop(0)
+                tmp.append(root.val)
+                if root.left: queue.append(root.left)
+                if root.right: queue.append(root.right)
+                size -= 1
+            ans.append(tmp)
+        return ans
+        
+        
