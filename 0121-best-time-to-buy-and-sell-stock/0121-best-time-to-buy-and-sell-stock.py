6959fd9be7ad9baa2644a26722c5b26a1f1f65a3
Sun, Chao-Ting <105558316+chaoting-sun@users.noreply.github.com>
2023-10-10 07:31:14 -0500

    Time: 912 ms (13.49%), Space: 27.3 MB (42.84%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -10,5 +10,4 @@ class Solution:
             minV = min(minV, prices[i])
         return maxProfit
 
-        
-        
+# nice source: https://leetcode.com/problems/best-time-to-buy-and-sell-stock/solutions/39038/kadane-s-algorithm-since-no-one-has-mentioned-about-this-so-far-in-case-if-interviewer-twists-the-input/?envType=list&envId=rab78cw1
