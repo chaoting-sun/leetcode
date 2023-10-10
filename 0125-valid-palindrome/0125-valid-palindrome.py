66ea82fd84d14aff6bd20247d62b4f88d25ccb67
Sun, Chao-Ting <105558316+chaoting-sun@users.noreply.github.com>
2023-10-10 08:08:22 -0500

    Time: 54 ms (38.97%), Space: 17 MB (77.09%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,15 +1,21 @@
+### method1: two pointers
+
+# time complexity: O(N)
+# space complexity: O(1)
+
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         n = len(s)
         left, right = 0, n - 1
 
         while left <= right:
-            while left < n and not s[left].isalnum():
+            # move to the next character that is alphameric
+            while left < right and not s[left].isalnum():
                 left += 1
-            while 0 <= right and not s[right].isalnum():
+            while left < right and not s[right].isalnum():
                 right -= 1
-            
-            if left > right: break
+            # if the left and right crossovered, stop
+            # if left > right: break
 
             if s[left].lower() != s[right].lower():
                 return False
