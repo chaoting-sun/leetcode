c63419ea77cb1394e798f0bdc01bb143809b8a9f
Sun, Chao-Ting <105558316+chaoting-sun@users.noreply.github.com>
2023-10-12 08:33:00 -0500

    Time: 140 ms (94.38%), Space: 17.8 MB (83.64%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,5 +1,8 @@
 ### method1: hash table
 
+# time complexity: O(N)
+# space complexity: O(N)
+
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         n = len(nums)
@@ -24,3 +27,21 @@ class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         nums.sort()
         return nums[len(nums)//2]
+
+
+### Boyer-Moore Majority Vote Algorithm
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        major = nums[0]
+        count = 1
+
+        for i in range(1, len(nums)):
+            if count == 0:
+                major = nums[i]
+                count += 1
+            elif nums[i] == major:
+                count += 1
+            else:
+                count -= 1
+        return major
