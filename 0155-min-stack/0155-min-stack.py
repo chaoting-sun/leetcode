83dcb59d089b0b41af1e650552318d40aae2632b
Sun, Chao-Ting <105558316+chaoting-sun@users.noreply.github.com>
2023-10-12 07:47:28 -0500

    Time: 66 ms (32.98%), Space: 20 MB (99%) - LeetHub

--- 0155-min-stack/0155-min-stack.py
+++ 0155-min-stack/0155-min-stack.py
@@ -1,33 +1,57 @@
-class Node:
-    def __init__(self, val, exist=True):
-        self.val = val
-        self.exist = exist
-    def __lt__(self, other):
-        return self.val < other.val
+### method1: stack + heap
+
+# class Node:
+#     def __init__(self, val, exist=True):
+#         self.val = val
+#         self.exist = exist
+#     def __lt__(self, other):
+#         return self.val < other.val
         
+
+# class MinStack:
+#     def __init__(self):
+#         self.stk = []
+#         self.minheap = []
+
+#     def push(self, val: int) -> None:
+#         node = Node(val)
+#         self.stk.append(node)
+#         heapq.heappush(self.minheap, node)       
+
+#     def pop(self) -> None:
+#         node = self.stk.pop()
+#         node.exist = False
+        
+#     def top(self) -> int:
+#         return self.stk[-1].val
+
+#     def getMin(self) -> int:
+#         while not self.minheap[0].exist:
+#             heapq.heappop(self.minheap)
+#         return self.minheap[0].val
+        
+
+### method2: 2 stacks
 
 class MinStack:
     def __init__(self):
         self.stk = []
-        self.minheap = []
+        self.minStk = []
 
     def push(self, val: int) -> None:
-        node = Node(val)
-        self.stk.append(node)
-        heapq.heappush(self.minheap, node)       
+        self.stk.append(val)
+        if not self.minStk or self.minStk[-1] >= val:
+            self.minStk.append(val)
 
     def pop(self) -> None:
-        node = self.stk.pop()
-        node.exist = False
-        
+        if self.minStk[-1] == self.stk.pop():
+            self.minStk.pop()
+
     def top(self) -> int:
-        return self.stk[-1].val
+        return self.stk[-1]
 
     def getMin(self) -> int:
-        while not self.minheap[0].exist:
-            heapq.heappop(self.minheap)
-        return self.minheap[0].val
-        
+        return self.minStk[-1]
 
 
 # Your MinStack object will be instantiated and called as such:
