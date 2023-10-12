ebc30266e893479f637f3efab0b0b0286318fb10
Sun, Chao-Ting <105558316+chaoting-sun@users.noreply.github.com>
2023-10-12 09:38:50 -0500

    Time: 1740 ms (5.06%), Space: 36.5 MB (5.22%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -0,0 +1,23 @@
+import numpy as np
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        count = 0
+        visited = np.zeros((len(grid), len(grid[0])))
+
+        def dfs(grid, i, j, visited):
+            visited[i][j] = 1
+            if i > 0 and grid[i-1][j] == '1' and not visited[i-1][j]: dfs(grid, i-1, j, visited)
+            if i < len(grid)-1 and grid[i+1][j] == '1' and not visited[i+1][j]: dfs(grid, i+1, j, visited)
+            if j > 0 and grid[i][j-1] == '1' and not visited[i][j-1]: dfs(grid, i, j-1, visited)
+            if j < len(grid[0])-1 and grid[i][j+1] == '1' and not visited[i][j+1]: dfs(grid, i, j+1, visited)
+
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == '1' and not visited[i][j]:
+                    print(count)
+                    dfs(grid, i, j, visited)
+                    print(visited)
+                    count += 1
+        return count
+                
+        
