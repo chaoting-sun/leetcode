cb62cd7658d58d5061800c48774893d00c96e766
Sun, Chao-Ting <105558316+chaoting-sun@users.noreply.github.com>
2023-10-13 04:06:28 -0500

    Time: 31 ms (97.54%), Space: 17.8 MB (58.37%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -3,6 +3,12 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
+### method1: use an array to store the nodes and traverse the node list twice
+
+# time complexity: O(N)
+# space complexity: O(N)
+
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         if not head or not head.next:
@@ -22,4 +28,20 @@ class Solution:
         return tail
 
 
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head
         
+        prevNode = None
+        currNode = head
+        nextNode = head.next
+
+        while nextNode:
+            currNode.next = prevNode
+            prevNode = currNode
+            currNode = nextNode
+            nextNode = nextNode.next
+        currNode.next = prevNode
+        return currNode
+
