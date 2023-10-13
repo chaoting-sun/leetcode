e2231cdf88ecd37000e8d35996b8ddb39af28b43
Sun, Chao-Ting <105558316+chaoting-sun@users.noreply.github.com>
2023-10-13 07:47:20 -0500

    Time: 103 ms (35.65%), Space: 19 MB (63.69%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.py
@@ -0,0 +1,60 @@
+### method1: dfs (TLE)
+
+# time complexity: O(E+V^2) (?)
+# space complexity: O(E+V)
+
+class Solution:
+    def isCyclic(self, courses, c, visited):
+        if visited[c]:
+            return True
+        
+        visited[c] = True
+        for i in courses[c]:
+            if self.isCyclic(courses, i, visited):
+                return True
+        visited[c] = False
+        return False
+
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        courses = [[] for i in range(numCourses)]
+        visited = [False] * numCourses
+
+        for a, b in prerequisites:
+            courses[b].append(a)
+
+        for i in range(numCourses):
+            if self.isCyclic(courses, i, visited):
+                return False
+        return True
+
+
+### method2: dfs + coloring algorithm
+
+# intuition: For a node, if all of its child nodes have been visited and found no cycles. Next time if this node are found from the other nodes, we can pass it.
+# source: https://leetcode.com/problems/course-schedule/solutions/658275/c-dfs-easiest-solution-with-explanation-my-1st-approach/?envType=list&envId=rab78cw1
+
+class Solution:
+    def isCyclic(self, courses, c, visited):
+        if visited[c] == 1:
+            return True
+        if visited[c] == 2:
+            return False
+
+        for i in courses[c]:
+            visited[c] = 1 # its current childs are being visited
+            if self.isCyclic(courses, i, visited):
+                return True
+        visited[c] = 2 # all of the childs of the node have been visited
+        return False
+
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        courses = [[] for i in range(numCourses)]
+        visited = [0] * numCourses
+
+        for a, b in prerequisites:
+            courses[b].append(a)
+
+        for i in range(numCourses):
+            if self.isCyclic(courses, i, visited):
+                return False
+        return True
