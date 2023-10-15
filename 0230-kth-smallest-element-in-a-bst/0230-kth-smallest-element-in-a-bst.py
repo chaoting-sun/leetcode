8bc99b815a3a6f551a37c92bee7de22866112bdd
Sun, Chao-Ting <105558316+chaoting-sun@users.noreply.github.com>
2023-10-15 17:09:43 +0800

    Time: 64 ms (6.45%), Space: 20.3 MB (71.95%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
@@ -4,6 +4,12 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+
+### method1: dfs
+# time complexity: O(N)
+# space complexity: O(N)
+
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
         def dfs(root):
@@ -11,3 +17,20 @@ class Solution:
                 return []
             return dfs(root.left)+[root.val]+dfs(root.right)
         return dfs(root)[k-1]
+
+
+### method2: binary search
+
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        def countNode(root):
+            if not root: return 0
+            return 1 + countNode(root.left) + countNode(root.right)
+        
+        cnt = countNode(root.left)
+        if cnt + 1 == k:
+            return root.val
+        elif cnt + 1 > k:
+            return self.kthSmallest(root.left, k)
+        else:
+            return self.kthSmallest(root.right, k - cnt - 1)
