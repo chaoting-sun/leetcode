3c3c2dd1928654785ece515ab86c4a8e8e286a9d
Sun, Chao-Ting <105558316+chaoting-sun@users.noreply.github.com>
2023-10-16 15:16:03 +0800

    Time: 188 ms (80.37%), Space: 25.2 MB (20.96%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,22 +1,20 @@
+### method1: two arrays: prefix product and suffix product
+
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        size = len(nums)
-        prefixProduct = [1] * size
-        suffixProduct = [1] * size
+        n = len(nums)
+        prefixProduct = [1] * n
+        suffixProduct = [1] * n
 
-        prefixProduct[0] = nums[0]
-        suffixProduct[size-1] = nums[size-1]
-
-        for i in range(1, size):
-            prefixProduct[i] = prefixProduct[i-1] * nums[i]
-        for i in range(size-2, -1, -1):
-            suffixProduct[i] = suffixProduct[i+1] * nums[i]
+        for i in range(1, n):
+            prefixProduct[i] = prefixProduct[i-1] * nums[i-1]
+        for i in range(n-2, -1, -1):
+            suffixProduct[i] = suffixProduct[i+1] * nums[i+1]
         
-        ans = [1] * size
-        ans[0], ans[size-1] = suffixProduct[1], prefixProduct[size-2]
-        for i in range(1, size-1):
-            ans[i] = prefixProduct[i-1] * suffixProduct[i+1]
+        ans = [1] * n
+        for i in range(n):
+            ans[i] = prefixProduct[i] * suffixProduct[i]
         return ans
-        
-        
-        
+
+
+### method2: 
