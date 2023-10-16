6e6c9cc25dc8b4832bf3ef4da85c809052ab6211
Sun, Chao-Ting <105558316+chaoting-sun@users.noreply.github.com>
2023-10-16 18:34:38 +0800

    Time: 446 ms (39.96%), Space: 38.8 MB (14.87%) - LeetHub

---
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
@@ -0,0 +1,30 @@
+class MedianFinder:
+    def __init__(self):
+        self.maxheap = [] # for small values
+        self.minheap = [] # for large values
+
+    def addNum(self, num: int) -> None:
+        heapq.heappush(self.maxheap, -num)
+
+        # check if max. value in maxheap is smaller than min. value in minheap
+        if self.maxheap and self.minheap and -self.maxheap[0] > self.minheap[0]:
+            heapq.heappush(self.minheap, -heapq.heappop(self.maxheap))
+        
+        # check if the number of elements on both heaps are even
+        if len(self.maxheap) - 1 > len(self.minheap):
+            heapq.heappush(self.minheap, -heapq.heappop(self.maxheap))
+        if len(self.minheap) - 1 > len(self.maxheap):
+            heapq.heappush(self.maxheap, -heapq.heappop(self.minheap))
+
+    def findMedian(self) -> float:
+        if len(self.maxheap) > len(self.minheap):
+            return -self.maxheap[0]
+        if len(self.maxheap) < len(self.minheap):
+            return self.minheap[0]
+        return (-self.maxheap[0] + self.minheap[0]) / 2
+
+
+# Your MedianFinder object will be instantiated and called as such:
+# obj = MedianFinder()
+# obj.addNum(num)
+# param_2 = obj.findMedian()
