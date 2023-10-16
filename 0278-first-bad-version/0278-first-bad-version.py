92cd7f5b6c29e8a21a9893220e38884f15d25a3f
Sun, Chao-Ting <105558316+chaoting-sun@users.noreply.github.com>
2023-10-16 16:19:49 +0800

    Time: 40 ms (41.46%), Space: 16.1 MB (96.4%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.py
+++ 0278-first-bad-version/0278-first-bad-version.py
@@ -3,7 +3,7 @@
 
 class Solution:
     def binarySearch(self, l, r):
-        c = int((l + r) // 2)
+        c = int(l + (r - l) / 2)
         if isBadVersion(c):
             if c == 1 or not isBadVersion(c-1):
                 return c
@@ -13,3 +13,16 @@ class Solution:
     
     def firstBadVersion(self, n: int) -> int:
         return self.binarySearch(1, n+1)
+
+
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        l, r = 1, n
+
+        while l < r:
+            mid = l + (r-l)//2
+            if isBadVersion(mid):
+                r = mid
+            else:
+                l = mid + 1
+        return l
