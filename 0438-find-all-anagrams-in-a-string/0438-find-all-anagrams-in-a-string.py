91dfa6298ce99b18a951894f3c85a951090aec9d
Sun, Chao-Ting <105558316+chaoting-sun@users.noreply.github.com>
2023-10-19 19:09:57 +0800

    Time: 100 ms (67.72%), Space: 17.3 MB (78.51%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -1,3 +1,6 @@
+### method: sliding window
+# source: https://www.youtube.com/watch?v=G8xtZy0fDKg
+
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
         if len(s) < len(p):
@@ -22,4 +25,44 @@ class Solution:
             
             if sCount == pCount:
                 res.append(l)
+        return res
+
+
+### method2: sliding window
+
+# source: https://leetcode.com/problems/find-all-anagrams-in-a-string/solutions/92007/sliding-window-algorithm-template-to-solve-all-the-leetcode-substring-search-problem/?envType=list&envId=rab78cw1
+
+class Solution:
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+        if len(s) < len(p):
+            return []
+
+        mp = {}
+        for c in p:
+            mp[c] = 1 + mp.get(c, 0)
+        
+        res = []
+        count = len(mp)
+        begin, end = 0, 0
+
+        while end < len(s):
+            c = s[end]
+
+            if c in mp:
+                mp[c] = mp.get(c, 0) - 1
+                if mp[c] == 0:
+                    count -= 1
+            
+            end += 1
+
+            while count == 0:
+                tempc = s[begin]
+                if tempc in mp:
+                    mp[tempc] += 1
+                    if mp[tempc] > 0:
+                        count += 1
+                
+                if end - begin == len(p):
+                    res.append(begin)
+                begin += 1
         return res
