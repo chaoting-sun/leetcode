1b00f8aea72dcf957e42605ab63b6c57b660d91a
Sun, Chao-Ting <105558316+chaoting-sun@users.noreply.github.com>
2023-10-19 23:57:34 +0800

    Time: 49 ms (52.97%), Space: 18.8 MB (18.46%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
@@ -1,3 +1,9 @@
+### method: dfs
+# to compute the diameter of the tree, we compute the diameters of all subtrees and find the largest
+# source: https://leetcode.com/problems/diameter-of-binary-tree/solutions/101132/java-solution-maxdepth/comments/104961
+# time complexity: O(N); N is the number of nodes.
+# space complexity: O(N) (N recursive calls)
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -15,3 +21,20 @@ class Solution:
             return max(leftHeight, rightHeight) + 1
         maxHeight(root)
         return self.diameter
+
+class Solution:
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        
+        def helper(root):
+            if not root: return 0, 0
+            
+            leftHeight, leftDiameter = helper(root.left)
+            rightHeight, rightDiameter = helper(root.right)
+
+            currentHeight = max(leftHeight, rightHeight) + 1 # leaves -> height = 1
+            currentDiameter = max(leftDiameter, rightDiameter, leftHeight+rightHeight)
+            
+            return currentHeight, currentDiameter
+
+        _, diameter = helper(root)
+        return diameter
