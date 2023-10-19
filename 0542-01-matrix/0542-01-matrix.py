63e59acc601c1b5ab165a51aa82227ee38500ba5
Sun, Chao-Ting <105558316+chaoting-sun@users.noreply.github.com>
2023-10-19 21:52:29 +0800

    Time: 534 ms (67.23%), Space: 19.1 MB (67.5%) - LeetHub

---
+++ 0542-01-matrix/0542-01-matrix.py
@@ -0,0 +1,64 @@
+### bfs; mine (TLE; O(N^2))
+
+class Solution:
+    def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
+        m, n = len(mat), len(mat[0])
+        ans = [[10E6 for _ in range(n)] for _ in range(m)]
+
+        def bfs(i, j):
+            visited = [[0 for _ in range(n)] for _ in range(m)]
+            q = deque([(i, j)])
+            distance = 0
+
+            while q:
+                size = len(q)
+                
+                for i in range(size):
+                    pi, pj = q.popleft()
+                    if mat[pi][pj] == 0:
+                        return distance
+
+                    visited[pi][pj] = 1
+
+                    if pi > 0 and not visited[pi-1][pj]: q.append((pi-1, pj))
+                    if pi < m - 1 and not visited[pi+1][pj]: q.append((pi+1, pj))
+                    if pj > 0 and not visited[pi][pj-1]: q.append((pi, pj-1))
+                    if pj < n - 1 and not visited[pi][pj+1]: q.append((pi, pj+1))
+                
+                distance += 1
+
+        for i in range(m):
+            for j in range(n):
+                if mat[i][j] == 0:
+                    ans[i][j] = 0
+                else:
+                    ans[i][j] = bfs(i, j)
+        return ans
+
+
+### bfs (smarter)
+
+class Solution:
+    def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
+        m, n = len(mat), len(mat[0])
+
+        q = deque()
+        for i in range(m):
+            for j in range(n):
+                if mat[i][j] == 0:
+                    q.append((i, j))
+                else:
+                    mat[i][j] = 10E8
+        
+        level = 0
+
+        while q:
+            i1, j1 = q.popleft()
+
+            for dx, dy in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+                i2 = i1 + dx
+                j2 = j1 + dy
+                if 0 <= i2 < m and 0 <= j2 < n and mat[i2][j2] > mat[i1][j1] + 1:
+                    mat[i2][j2] = mat[i1][j1] + 1
+                    q.append((i2, j2))
+        return mat
