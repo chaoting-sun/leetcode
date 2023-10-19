ccc17cc6b25185efa8bd6fc55162be6afc521095
Sun, Chao-Ting <105558316+chaoting-sun@users.noreply.github.com>
2023-10-19 22:13:32 +0800

    Time: 455 ms (94.02%), Space: 19.2 MB (65.18%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.py
+++ 0542-01-matrix/0542-01-matrix.py
@@ -101,3 +101,26 @@ class Solution:
             level += 1
         
         return mat
+
+
+### dp
+
+class Solution:
+    def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
+        m, n = len(mat), len(mat[0])
+        maxv = 10E6
+
+        for i in range(m):
+            for j in range(n):
+                if mat[i][j] != 0:
+                    top = mat[i-1][j] if i > 0 else maxv
+                    left = mat[i][j-1] if j > 0 else maxv
+                    mat[i][j] = min(top, left) + 1
+        
+        for j in range(n-1, -1, -1):
+            for i in range(m-1, -1, -1):
+                if mat[i][j] != 0:
+                    bottom = mat[i+1][j] if i < m-1 else maxv
+                    right = mat[i][j+1] if j < n-1 else maxv
+                    mat[i][j] = min(mat[i][j], min(bottom, right) + 1)
+        return mat
