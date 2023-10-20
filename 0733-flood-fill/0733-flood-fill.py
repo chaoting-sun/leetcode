c07389651b16eba41d100e61a0d8a1964e894bda
Sun, Chao-Ting <105558316+chaoting-sun@users.noreply.github.com>
2023-10-20 18:47:52 +0800

    Time: 74 ms (64.12%), Space: 16.4 MB (57.66%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -27,7 +27,7 @@ class Solution:
         return image
 
 
-### method1.1: bfs; optimized
+### method1.1: bfs; remove visited
 
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
@@ -56,18 +56,27 @@ class Solution:
 
 ### method2: dfs
 
-# class Solution:
-#     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
-#         if image[sr][sc] == color:
-#             return image
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        if image[sr][sc] == color:
+            return image
 
-#         m, n = len(image), len(image[0])
-#         stack = [(sr, sc)]
-#         startColor = image[sr][sc]
-#         visited = [[False for _ in range(n)] for _ in range(m)]
+        m, n = len(image), len(image[0])
+        stack = [(sr, sc)]
+        startColor = image[sr][sc]
+        visited = [[False for _ in range(n)] for _ in range(m)]
 
-#         while stack:
-#             x, y = stack.pop()
-#             image[x][y] = color
-#             visited[x][y] = True
+        while stack:
+            x, y = stack.pop()
+            image[x][y] = color
+            visited[x][y] = True
+
+            for dx, dy in [(1, 0), (0, 1), (-1, 0), (0, -1)]:
+                    newx, newy = x + dx, y + dy
+                    if not 0 <= newx < m or not 0 <= newy < n:
+                        continue
+                    if image[newx][newy] == startColor:
+                        stack.append((newx, newy))
+        return image
+
 
