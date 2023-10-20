85b5fceae96298d86325afc1c9ce07f4378ead93
Sun, Chao-Ting <105558316+chaoting-sun@users.noreply.github.com>
2023-10-20 18:11:41 +0800

    Time: 188 ms (86.67%), Space: 20.6 MB (77.17%) - LeetHub

--- 0721-accounts-merge/0721-accounts-merge.py
+++ 0721-accounts-merge/0721-accounts-merge.py
@@ -1,4 +1,7 @@
-### method: dfs; hashmap
+### method1: dfs; hashmap
+
+# time complexity: O(# emails + len(accounts))
+# space complexity: O(# emails + len(accounts))
 
 class Solution:
     def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
@@ -28,3 +31,53 @@ class Solution:
             ans[i] = [ans[i][0]] + sorted(ans[i][1:])
 
         return ans
+
+
+### method2: union find
+
+# source: https://www.youtube.com/watch?v=6st4IxEF-90
+
+class UnionFind:
+    def __init__(self, n):
+        self.parent = [i for i in range(n)]
+        self.rank = [1] * n
+    
+    def find(self, x):
+        while x != self.parent[x]:
+            self.parent[x] = self.parent[self.parent[x]]
+            x = self.parent[x]
+        return x
+
+    def union(self, x1, x2):
+        p1, p2 = self.find(x1), self.find(x2)
+        if p1 == p2:
+            return False
+        elif self.rank[p1] < self.rank[p2]:
+            self.parent[p1] = p2
+            self.rank[p2] += self.rank[p1]
+        else:
+            self.parent[p2] = p1
+            self.rank[p1] += self.rank[p2]
+
+class Solution:
+    def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
+        uf = UnionFind(len(accounts))
+        emailToAccount = {}
+
+        for i, a in enumerate(accounts):
+            for e in a[1:]:
+                if e in emailToAccount:
+                    uf.union(i, emailToAccount[e])
+                else:
+                    emailToAccount[e] = i
+
+        accountToEmails = defaultdict(list)
+        for e, i in emailToAccount.items():
+            leader = uf.find(i)
+            accountToEmails[leader].append(e)
+        
+        res = []
+        for i, emails in accountToEmails.items():
+            name = accounts[i][0]
+            res.append([name]+sorted(emails))
+        return res
