6cf31daa739f3481a9e52181ea0a93a8b2b76cde
Sun, Chao-Ting <105558316+chaoting-sun@users.noreply.github.com>
2023-10-20 19:01:28 +0800

    Time: 215 ms (51.48%), Space: 17.9 MB (14.16%) - LeetHub

--- 0792-binary-search/0792-binary-search.py
+++ 0792-binary-search/0792-binary-search.py
@@ -1,8 +1,11 @@
+# method: binary search
+
+# time complexity = O(logN)
+# space complexity: O(1)
+
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         l, r = 0, len(nums) - 1
-        # if not nums[l] <= target <= nums[r]:
-        #     return -1
 
         while l <= r:
             c = l + (r-l)//2
