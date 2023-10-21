a398650f99c235f1cf5dd2e9914e036bb5fddef6
Sun, Chao-Ting <105558316+chaoting-sun@users.noreply.github.com>
2023-10-21 17:37:18 +0800

    Time: 83 ms (55.51%), Space: 19.7 MB (88.05%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -1,55 +1,45 @@
 ### method1
 
-# class Solution(object):
-#     def insert(self, intervals, newInterval):
-#         """
-#         :type intervals: List[List[int]]
-#         :type newInterval: List[int]
-#         :rtype: List[List[int]]
-#         """
-#         n = len(intervals)
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        n = len(intervals)
 
-#         if n == 0:
-#             return [newInterval]
+        if n == 0:
+            return [newInterval]
         
-#         head = 0
-#         tail = n - 1
-#         ans = []
+        head = 0
+        tail = n - 1
+        ans = []
 
-#         while head < n and newInterval[0] > intervals[head][1]:
-#             head += 1
+        while head < n and newInterval[0] > intervals[head][1]:
+            head += 1
 
-#         while tail >= 0 and newInterval[1] < intervals[tail][0]:
-#             tail -= 1
+        while tail >= 0 and newInterval[1] < intervals[tail][0]:
+            tail -= 1
 
-#         # left
-#         if head > 0:
-#             ans = ans + intervals[:head]
+        # left
+        if head > 0:
+            ans = ans + intervals[:head]
         
-#         # center
-#         if head == n:
-#             ans = ans + [newInterval]
-#         elif tail == -1:
-#             ans = [newInterval] + ans
-#         else:
-#             ans = ans + [[min(intervals[head][0], newInterval[0]), max(intervals[tail][1], newInterval[1])]]
+        # center
+        if head == n:
+            ans = ans + [newInterval]
+        elif tail == -1:
+            ans = [newInterval] + ans
+        else:
+            ans = ans + [[min(intervals[head][0], newInterval[0]), max(intervals[tail][1], newInterval[1])]]
         
-#         # right
-#         if tail + 1 < n:
-#             ans = ans + intervals[tail+1:]
+        # right
+        if tail + 1 < n:
+            ans = ans + intervals[tail+1:]
 
-#         return ans
+        return ans
 
 
 ### method2
 
-class Solution(object):
-    def insert(self, intervals, newInterval):
-        """
-        :type intervals: List[List[int]]
-        :type newInterval: List[int]
-        :rtype: List[List[int]]
-        """
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
         ans = []
 
         i = 0
