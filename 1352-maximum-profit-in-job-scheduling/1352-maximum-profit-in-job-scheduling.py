b9803f835044c7f489f4b0a41109b0f95edb4506
Sun, Chao-Ting <105558316+chaoting-sun@users.noreply.github.com>
2023-10-21 16:28:33 +0800

    Time: 527 ms (35.96%), Space: 45.1 MB (29.95%) - LeetHub

---
+++ 1352-maximum-profit-in-job-scheduling/1352-maximum-profit-in-job-scheduling.py
@@ -0,0 +1,27 @@
+### method: dynamic programming
+
+from numpy import argsort
+class Solution:
+    def jobScheduling(self, startTime: List[int], endTime: List[int], profit: List[int]) -> int:
+        n = len(startTime)
+
+        sorted_ids = argsort(endTime)
+        startTime = [startTime[i] for i in sorted_ids]
+        endTime = [endTime[i] for i in sorted_ids]
+        profit = [profit[i] for i in sorted_ids]
+
+        p = [-1] * n
+        for i in range(1, n):
+            for j in range(i-1, -1, -1):
+                if endTime[j] <= startTime[i]:
+                    p[i] = j
+                    break
+
+        opt = [0] * (n+1)
+
+        for i in range(1, n+1):
+            profitWithCurr = profit[i-1]
+            if p[i-1] != -1:
+                profitWithCurr += opt[p[i-1]+1]
+            opt[i] = max(profitWithCurr, opt[i-1])
+        return opt[n]
