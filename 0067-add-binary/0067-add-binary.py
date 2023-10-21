b86a2e8d03e84256c44d1e5373c7b39524a4bdbe
Sun, Chao-Ting <105558316+chaoting-sun@users.noreply.github.com>
2023-10-21 17:39:33 +0800

    Time: 47 ms (21.46%), Space: 16.3 MB (58.52%) - LeetHub

--- 0067-add-binary/0067-add-binary.py
+++ 0067-add-binary/0067-add-binary.py
@@ -1,40 +1,40 @@
 ### mine
 
-# class Solution(object):
-#     def addBinary(self, a: str, b: str) -> str:
-#         ans = ''
-#         i, j = len(a)-1, len(b)-1
-#         tmp = 0
+class Solution(object):
+    def addBinary(self, a: str, b: str) -> str:
+        ans = ''
+        i, j = len(a)-1, len(b)-1
+        tmp = 0
 
-#         while i >= 0 or j >= 0:
-#             print(i, j)
-#             if i < 0:
-#                tmp += int(b[j])
-#                j -= 1
-#             elif j < 0:
-#                 tmp += int(a[i])
-#                 i -= 1
-#             else:
-#                 tmp += int(a[i]) + int(b[j])
-#                 i -= 1
-#                 j -= 1
+        while i >= 0 or j >= 0:
+            print(i, j)
+            if i < 0:
+               tmp += int(b[j])
+               j -= 1
+            elif j < 0:
+                tmp += int(a[i])
+                i -= 1
+            else:
+                tmp += int(a[i]) + int(b[j])
+                i -= 1
+                j -= 1
             
-#             if tmp == 0:
-#                 ans = '0' + ans
-#             elif tmp == 1:
-#                 ans = '1' + ans
-#                 tmp = 0
-#             elif tmp == 2:
-#                 ans = '0' + ans
-#                 tmp = 1
-#             elif tmp == 3:
-#                 ans = '1' + ans
-#                 tmp = 1
+            if tmp == 0:
+                ans = '0' + ans
+            elif tmp == 1:
+                ans = '1' + ans
+                tmp = 0
+            elif tmp == 2:
+                ans = '0' + ans
+                tmp = 1
+            elif tmp == 3:
+                ans = '1' + ans
+                tmp = 1
 
-#         if tmp == 1:
-#             return '1' + ans
-#         else:
-#             return ans
+        if tmp == 1:
+            return '1' + ans
+        else:
+            return ans
 
 
 ### cleaner
