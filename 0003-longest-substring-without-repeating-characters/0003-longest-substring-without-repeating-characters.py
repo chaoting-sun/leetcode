687fe7d4ac4d5e466de350f19489ca8f52427440
Sun, Chao-Ting <105558316+chaoting-sun@users.noreply.github.com>
2023-10-21 16:53:51 +0800

    Time: 44 ms (98.79%), Space: 16.4 MB (26.01%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,57 +1,45 @@
 ### method1: hashmap
 
-# class Solution(object):
-#     def lengthOfLongestSubstring(self, s):
-#         """
-#         :type s: str
-#         :rtype: int
-#         """
-#         chars = {}
-#         max_len = 0
-#         left = 0
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        chars = {}
+        max_len = 0
+        left = 0
 
-#         for right in range(len(s)):
-#             if s[right] in chars:
-#                 max_len = max(max_len, right-left)
-#                 if chars[s[right]] >= left:
-#                     left = chars[s[right]] + 1
-#             chars[s[right]] = right
+        for right in range(len(s)):
+            if s[right] in chars:
+                max_len = max(max_len, right-left)
+                if chars[s[right]] >= left:
+                    left = chars[s[right]] + 1
+            chars[s[right]] = right
 
-#         return max(max_len, len(s)-left)
+        return max(max_len, len(s)-left)
 
 
 ### method2: set
 
-# class Solution(object):
-#     def lengthOfLongestSubstring(self, s):
-#         """
-#         :type s: str
-#         :rtype: int
-#         """
-#         left = right = 0
-#         chars = set()
-#         max_len = 0
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        left = right = 0
+        chars = set()
+        max_len = 0
 
-#         for right in range(len(s)):
-#             if s[right] not in chars:
-#                 chars.add(s[right])
-#                 max_len = max(max_len, right - left + 1)
-#             else:
-#                 while s[right] in chars:
-#                     chars.remove(s[left])
-#                     left += 1
-#                 chars.add(s[right])
-#         return max_len
+        for right in range(len(s)):
+            if s[right] not in chars:
+                chars.add(s[right])
+                max_len = max(max_len, right - left + 1)
+            else:
+                while s[right] in chars:
+                    chars.remove(s[left])
+                    left += 1
+                chars.add(s[right])
+        return max_len
 
 
 ### method3: integer array
 
-class Solution(object):
-    def lengthOfLongestSubstring(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
         left = right = 0
         max_len = 0
         chars = [-1] * 128
