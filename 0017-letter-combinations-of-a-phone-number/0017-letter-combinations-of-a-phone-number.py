a152fe7b95d0574d2937064ac74656a9b786eb9f
Sun, Chao-Ting <105558316+chaoting-sun@users.noreply.github.com>
2023-10-21 17:08:18 +0800

    Time: 38 ms (57.43%), Space: 16.4 MB (33.43%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,83 +1,71 @@
 ### method1: fill characters in order (mine)
 
-# class Solution(object):
-#     def letterCombinations(self, digits):
-#         """
-#         :type digits: str
-#         :rtype: List[str]
-#         """
-#         if len(digits) == 0:
-#             return []
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if len(digits) == 0:
+            return []
 
-#         d2e = {
-#             '2': ['a', 'b', 'c'],
-#             '3': ['d', 'e', 'f'],
-#             '4': ['g', 'h', 'i'],
-#             '5': ['j', 'k', 'l'],
-#             '6': ['m', 'n', 'o'],
-#             '7': ['p', 'q', 'r', 's'],
-#             '8': ['t', 'u', 'v'],
-#             '9': ['w', 'x', 'y', 'z']
-#         }
+        d2e = {
+            '2': ['a', 'b', 'c'],
+            '3': ['d', 'e', 'f'],
+            '4': ['g', 'h', 'i'],
+            '5': ['j', 'k', 'l'],
+            '6': ['m', 'n', 'o'],
+            '7': ['p', 'q', 'r', 's'],
+            '8': ['t', 'u', 'v'],
+            '9': ['w', 'x', 'y', 'z']
+        }
 
-#         combs = [d2e[d] for d in digits]
+        combs = [d2e[d] for d in digits]
         
-#         n_total_comb = 1
-#         for c in combs:
-#             n_total_comb *= len(c)
+        n_total_comb = 1
+        for c in combs:
+            n_total_comb *= len(c)
         
-#         n_comb_each = []
-#         n_curr_comb = n_total_comb
-#         for c in combs:
-#             n_curr_comb /= len(c)
-#             n_comb_each.append(n_curr_comb)
+        n_comb_each = []
+        n_curr_comb = n_total_comb
+        for c in combs:
+            n_curr_comb /= len(c)
+            n_comb_each.append(n_curr_comb)
 
-#         ans = ['' for i in range(n_total_comb)]
+        ans = ['' for i in range(n_total_comb)]
         
-#         for i in range(len(combs)):
-#             s = ''
-#             for j in range(len(combs[i])):
-#                 s += combs[i][j] * n_comb_each[i]
-#             total_s = s * (n_total_comb / len(s))
+        for i in range(len(combs)):
+            s = ''
+            for j in range(len(combs[i])):
+                s += combs[i][j] * n_comb_each[i]
+            total_s = s * (n_total_comb / len(s))
 
-#             for k, s in enumerate(total_s):
-#                 ans[k] += s
+            for k, s in enumerate(total_s):
+                ans[k] += s
 
-#         return ans
+        return ans
 
 
 ### method2: BFS
 
-# class Solution(object):
-#     def letterCombinations(self, digits):
-#         """
-#         :type digits: str
-#         :rtype: List[str]
-#         """
-#         if len(digits) == 0:
-#             return []
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if len(digits) == 0:
+            return []
 
-#         chars = ['', '', 'abc', 'def', 'ghi', 'jkl',
-#                  'mno', 'pqrs', 'tuv', 'wxyz']
-#         ans = ['']
+        chars = ['', '', 'abc', 'def', 'ghi', 'jkl',
+                 'mno', 'pqrs', 'tuv', 'wxyz']
+        ans = ['']
 
-#         for d in digits:
-#             tmp = []
-#             for a in ans:
-#                 for e in chars[ord(d)-ord('0')]:
-#                     tmp.append(a + e)
-#             ans = tmp
-#         return ans
+        for d in digits:
+            tmp = []
+            for a in ans:
+                for e in chars[ord(d)-ord('0')]:
+                    tmp.append(a + e)
+            ans = tmp
+        return ans
 
 
 ### method3: DFS
 
 class Solution:
-    def letterCombinations(self, digits):
-        """
-        :type digits: str
-        :rtype: List[str]
-        """
+    def letterCombinations(self, digits: str) -> List[str]:
         chars = ['', '', 'abc', 'def', 'ghi', 'jkl',
                  'mno', 'pqrs', 'tuv','wxyz']
 
