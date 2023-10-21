d62b349a7fdcd7b5493b587ed6d86e6774e03990
Sun, Chao-Ting <105558316+chaoting-sun@users.noreply.github.com>
2023-10-21 17:26:53 +0800

    Time: 107 ms (81.97%), Space: 18.2 MB (50.69%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,38 +1,30 @@
 # method1: dp
 
-# class Solution(object):
-#     def trap(self, height):
-#         """
-#         :type height: List[int]
-#         :rtype: int
-#         """
-#         n = len(height)
-#         prefixMax = [0] * n
-#         suffixMax = [0] * n
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        n = len(height)
+        prefixMax = [0] * n
+        suffixMax = [0] * n
         
-#         maxV = 0
-#         for i in range(n):
-#             maxV = max(maxV, height[i])
-#             prefixMax[i] = maxV
-#         maxV = 0
-#         for i in range(n-1, -1, -1):
-#             maxV = max(maxV, height[i])
-#             suffixMax[i] = maxV
+        maxV = 0
+        for i in range(n):
+            maxV = max(maxV, height[i])
+            prefixMax[i] = maxV
+        maxV = 0
+        for i in range(n-1, -1, -1):
+            maxV = max(maxV, height[i])
+            suffixMax[i] = maxV
 
-#         vol = 0
-#         for h, pref, suff in zip(height, prefixMax, suffixMax):
-#             vol += min(pref, suff) - h
-#         return vol
+        vol = 0
+        for h, pref, suff in zip(height, prefixMax, suffixMax):
+            vol += min(pref, suff) - h
+        return vol
 
 
 # method2: two pointers
 
-class Solution(object):
-    def trap(self, height):
-        """
-        :type height: List[int]
-        :rtype: int
-        """
+class Solution:
+    def trap(self, height: List[int]) -> int:
         left, right = 0, len(height) - 1
         leftMax = rightMax = 0
         vol = 0
@@ -48,10 +40,3 @@ class Solution(object):
                 vol += leftMax - height[left]
                 left += 1
         return vol
-
-
-            
-        
-
-
-        
