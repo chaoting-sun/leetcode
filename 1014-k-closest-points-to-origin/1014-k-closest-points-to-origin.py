6e58faed9ffa77c9b36c21409808586a11afda3f
Sun, Chao-Ting <105558316+chaoting-sun@users.noreply.github.com>
2023-10-21 11:17:18 +0800

    Time: 779 ms (11.01%), Space: 38.8 MB (5.57%) - LeetHub

--- 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.py
+++ 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.py
@@ -1,7 +1,28 @@
-from numpy import argsort
+### method1: sort
+# time complexity: O(NlogN)
+# space complexity: O(N)
 
+from numpy import argsort
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
         dis = [x**2+y**2 for x, y in points]
         smallIndices = argsort(dis)[:k]
         return [points[i] for i in smallIndices]
+
+
+### method2: heap
+# time complexity:
+# space complexity
+
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        dis = [x**2+y**2 for x, y in points]
+        maxheap = []
+        for i, d in enumerate(dis):
+            heapq.heappush(maxheap, (-d, points[i]))
+            if len(maxheap) > k:
+                heapq.heappop(maxheap)
+        ans = []
+        while maxheap:
+            ans.append(heapq.heappop(maxheap)[1])
+        return ans
