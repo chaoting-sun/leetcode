20c6fa1d74f9a2d0bc92d75dec02155aab9b000f
Sun, Chao-Ting <105558316+chaoting-sun@users.noreply.github.com>
2023-10-21 17:19:12 +0800

    Time: 42 ms (88.62%), Space: 16.7 MB (50.81%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,53 +1,43 @@
 # method1
 
-# class Solution(object):
-#     def binarySearch(self, nums, l, r, target):
-#         while l <= r:
-#             c = (l + r) // 2
-#             if nums[c] == target:
-#                 return c
-#             elif nums[c] > target:
-#                 r = c - 1
-#             elif nums[c] < target:
-#                 l = c + 1
-#         return -1
+class Solution(object):
+    def binarySearch(self, nums, l, r, target):
+        while l <= r:
+            c = (l + r) // 2
+            if nums[c] == target:
+                return c
+            elif nums[c] > target:
+                r = c - 1
+            elif nums[c] < target:
+                l = c + 1
+        return -1
 
-#     def search(self, nums, target):
-#         """
-#         :type nums: List[int]
-#         :type target: int
-#         :rtype: int
-#         """
-#         n = len(nums)
+    def search(self, nums: List[int], target: int) -> int:
+        n = len(nums)
 
-#         l, r = 0, n - 1
+        l, r = 0, n - 1
 
-#         while l < r:
-#             c = (l + r) // 2
-#             if nums[c] < nums[r]:
-#                 r = c
-#             else:
-#                 l = c + 1
-#         pivot = l
+        while l < r:
+            c = (l + r) // 2
+            if nums[c] < nums[r]:
+                r = c
+            else:
+                l = c + 1
+        pivot = l
 
-#         if pivot == 0:
-#             return self.binarySearch(nums, 0, n-1, target)
-#         else:
-#             if nums[0] <= target <= nums[pivot-1]:
-#                 return self.binarySearch(nums, 0, pivot-1, target)
-#             else:
-#                 return self.binarySearch(nums, pivot, n-1, target)
+        if pivot == 0:
+            return self.binarySearch(nums, 0, n-1, target)
+        else:
+            if nums[0] <= target <= nums[pivot-1]:
+                return self.binarySearch(nums, 0, pivot-1, target)
+            else:
+                return self.binarySearch(nums, pivot, n-1, target)
 
 
 # method2 (hard to understand)
 
-class Solution(object):
-    def search(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: int
-        """
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
         n = len(nums)
 
         l, r = 0, n - 1
