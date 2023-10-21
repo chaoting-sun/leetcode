c1b3b56e3259b8aa2e1bd80670226e76ed2978ec
Sun, Chao-Ting <105558316+chaoting-sun@users.noreply.github.com>
2023-10-21 17:01:58 +0800

    Time: 75 ms (7.19%), Space: 33 MB (20.89%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
@@ -1,70 +1,61 @@
 ### method1: mine
 
-# import numpy as np
+import numpy as np
 
-# class Solution(object):
-#     def myAtoi(self, s):
-#         """
-#         :type s: str
-#         :rtype: int
-#         """
-#         valid_char = ['0', '1', '2', '3', '4',
-#                       '5', '6', '7', '8', '9']
+class Solution:
+    def myAtoi(self, s: str) -> int:
+        valid_char = ['0', '1', '2', '3', '4',
+                      '5', '6', '7', '8', '9']
+        sign = 0
+        p = 0
 
-#         sign = 0
-#         p = 0
+        while p < len(s):
+            if s[p] == ' ':
+                if sign != 0:
+                    return 0
+                p += 1
+                continue
+            elif s[p] == '+' or s[p] == '-':
+                if sign == 0:
+                    sign = 1 if s[p] == '+' else -1
+                else:
+                    return 0
+                p += 1
+                continue
+            elif s[p] in valid_char:
+                break
+            else: # other character
+                return 0
 
-#         while p < len(s):
-#             if s[p] == ' ':
-#                 if sign != 0:
-#                     return 0
-#                 p += 1
-#                 continue
-#             elif s[p] == '+' or s[p] == '-':
-#                 if sign == 0:
-#                     sign = 1 if s[p] == '+' else -1
-#                 else:
-#                     return 0
-#                 p += 1
-#                 continue
-#             elif s[p] in valid_char:
-#                 break
-#             else: # other character
-#                 return 0
+        digits = []
 
-#         digits = []
+        for i in range(p, len(s)):
+            try:
+                digits.append(int(s[i]))
+            except:
+                break
 
-#         for i in range(p, len(s)):
-#             try:
-#                 digits.append(int(s[i]))
-#             except:
-#                 break
+        ans = 0
+        ub = 2**31-1
+        lb = -2**31
 
-#         ans = 0
-#         ub = 2**31-1
-#         lb = -2**31
+        for i in range(len(digits)):
+            ans += digits[i] * 10**(len(digits)-i-1)
 
-#         for i in range(len(digits)):
-#             ans += digits[i] * 10**(len(digits)-i-1)
+        ans = sign*ans if sign == -1 else ans
 
-#         ans = sign*ans if sign == -1 else ans
-
-#         if ans > ub:
-#             return ub
-#         elif ans < lb:
-#             return lb
-#         else:
-#             return ans
+        if ans > ub:
+            return ub
+        elif ans < lb:
+            return lb
+        else:
+            return ans
 
 
 ### method2: faster
 
-class Solution(object):
-    def myAtoi(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
+class Solution:
+    def myAtoi(self, s: str) -> int:
         p = 0
         n = len(s)
 
