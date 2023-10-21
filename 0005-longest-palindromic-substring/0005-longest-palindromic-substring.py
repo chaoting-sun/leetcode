afd1ccc212395e059c2143b8b2e5a6403ebeb216
Sun, Chao-Ting <105558316+chaoting-sun@users.noreply.github.com>
2023-10-21 16:57:39 +0800

    Time: 365 ms (91.79%), Space: 16.4 MB (56.77%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,72 +1,63 @@
 ### method1: two stacks
 
-# class Solution(object):
-#     def longestPalindrome(self, s):
-#         """
-#         :type s: str
-#         :rtype: str
-#         """
-#         stk1 = []
-#         stk2 = []
-#         curr = right = 0
-#         max_len = 1
-#         ans = s[0]
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        stk1 = []
+        stk2 = []
+        curr = right = 0
+        max_len = 1
+        ans = s[0]
 
-#         while curr < len(s):
-#             # even
-#             if len(stk1) >= 1 and stk1[-1] == s[curr]:
-#                 right = curr
-#                 while len(stk1) and right < len(s) and stk1[-1] == s[right]:
-#                     stk2.append(stk1.pop(-1))
-#                     right += 1
+        while curr < len(s):
+            # even
+            if len(stk1) >= 1 and stk1[-1] == s[curr]:
+                right = curr
+                while len(stk1) and right < len(s) and stk1[-1] == s[right]:
+                    stk2.append(stk1.pop(-1))
+                    right += 1
 
-#                 curr_len = 2 * (right - curr)
-#                 if curr_len > max_len:
-#                     max_len = curr_len
-#                     ans = s[len(stk1):right]
+                curr_len = 2 * (right - curr)
+                if curr_len > max_len:
+                    max_len = curr_len
+                    ans = s[len(stk1):right]
 
-#                 while len(stk2):
-#                     stk1.append(stk2.pop(-1))
+                while len(stk2):
+                    stk1.append(stk2.pop(-1))
 
-#             # odd
-#             if len(stk1) >= 2 and stk1[-2] == s[curr]:
-#                 right = curr
-#                 stk2.append(stk1.pop(-1))
-#                 while len(stk1) and right < len(s) and stk1[-1] == s[right]:
-#                     stk2.append(stk1.pop(-1))
-#                     right += 1
+            # odd
+            if len(stk1) >= 2 and stk1[-2] == s[curr]:
+                right = curr
+                stk2.append(stk1.pop(-1))
+                while len(stk1) and right < len(s) and stk1[-1] == s[right]:
+                    stk2.append(stk1.pop(-1))
+                    right += 1
 
-#                 curr_len = 2 * (right - curr) + 1
-#                 if curr_len > max_len:
-#                     max_len = curr_len
-#                     ans = s[len(stk1):right]
+                curr_len = 2 * (right - curr) + 1
+                if curr_len > max_len:
+                    max_len = curr_len
+                    ans = s[len(stk1):right]
 
-#                 while len(stk2):
-#                     stk1.append(stk2.pop(-1))
+                while len(stk2):
+                    stk1.append(stk2.pop(-1))
 
-#             stk1.append(s[curr])
-#             curr += 1
+            stk1.append(s[curr])
+            curr += 1
 
-#         return ans
+        return ans
 
 
 ### method2: expand around center (EAC)
 
-class Solution(object):
+class Solution:
     def substr(self, s, l, r):
         while l >= 0 and r < len(s) and s[l] == s[r]:
             l -= 1
             r += 1
         return l, r
 
-    def longestPalindrome(self, s):
-        """
-        :type s: str
-        :rtype: str
-        """
+    def longestPalindrome(self, s: str) -> str:
         if len(s) == 1:
             return s
-        
         ans = ""
         max_len = 0
 
@@ -90,12 +81,8 @@ class Solution(object):
 
 # import numpy as np
 
-# class Solution(object):
-#     def longestPalindrome(self, s):
-#         """
-#         :type s: str
-#         :rtype: str
-#         """
+# class Solution:
+#     def longestPalindrome(self, s: str) -> str:
 #         ans = s[0]
 #         max_len = 1
 #         dp = np.ones((len(s), len(s))) # default: True
