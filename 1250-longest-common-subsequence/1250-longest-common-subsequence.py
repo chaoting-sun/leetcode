f1df2dde53158e2c8a3b2624e65850fefaded8d4
Sun, Chao-Ting <105558316+chaoting-sun@users.noreply.github.com>
2023-10-22 17:01:17 +0800

    Time: 661 ms (46.8%), Space: 16.4 MB (95.16%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.py
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.py
@@ -18,3 +18,21 @@ class Solution:
                 else:
                     dp[i][j] = max(dp[i-1][j], dp[i][j-1])
         return dp[m][n]
+
+
+### method: dp + optimized space
+
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        m, n = len(text1), len(text2)
+        dp = [[0 for _ in range(n+1)] for _ in range(2)]
+
+        for i in range(1, m+1):
+            for j in range(1, n+1):
+                curri = i % 2
+                previ = 0 if curri == 1 else 1 
+                if text1[i-1] == text2[j-1]:
+                    dp[curri][j] = 1 + dp[previ][j-1]
+                else:
+                    dp[curri][j] = max(dp[previ][j], dp[curri][j-1])
+        return dp[m%2][n]
