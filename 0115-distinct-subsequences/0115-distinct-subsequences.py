380d12328363f71ab504ea88b4e994e9b46dce18
Sun, Chao-Ting <105558316+chaoting-sun@users.noreply.github.com>
2023-10-22 18:11:44 +0800

    Time: 578 ms (29.34%), Space: 73.6 MB (42.24%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.py
+++ 0115-distinct-subsequences/0115-distinct-subsequences.py
@@ -1,7 +1,9 @@
+### method1: dp (top to bottom)
+
 class Solution:
     def numDistinct(self, s: str, t: str) -> int:
         cache = {}
-        def dfs(i, j):
+        def topToBottom(i, j):
             if j < 0: # has found all characters in t
                 return 1
             elif i < 0: # has not found all characters in t
@@ -15,5 +17,33 @@ class Solution:
                 cache[(i, j)] = dfs(i-1, j)
             return cache[(i, j)]
         
-        dfs(len(s)-1, len(t)-1)
+        topToBottom(len(s)-1, len(t)-1)
         return cache[(len(s)-1, len(t)-1)]
+
+
+### method2: dp (bottom to top)
+
+class Solution:
+    def numDistinct(self, s: str, t: str) -> int:
+        m, n = len(s), len(t)
+
+        dp = [[0 for _ in range(n+1)] for _ in range(m+1)]
+        for i in range(m+1):
+            dp[i][0] = 1
+        
+        for i in range(1, m+1):
+            for j in range(1, n+1):
+                if s[i-1] == t[j-1]:
+                    """
+                    if si == tj (s[i-1] == t[j-1]), we should consider two conds:
+                    1. there may be k < i s.t. sk == tj. so we ignore si -> dp[i-1][j]
+                    2. we match si and tj, and consider the combinations before
+                    i and j -> dp[i-1][j-1]
+                    """
+                    dp[i][j] = dp[i-1][j-1] + dp[i-1][j]
+                else:
+                    """
+                    si can be ignored, so we use the record dp[i-1][j]
+                    """
+                    dp[i][j] = dp[i-1][j]
+        return dp[m][n]
