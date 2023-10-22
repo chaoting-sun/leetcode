bff824280d4cda6bde69d1509563569354363719
Sun, Chao-Ting <105558316+chaoting-sun@users.noreply.github.com>
2023-10-22 19:35:29 +0800

    Time: 81 ms (46.02%), Space: 24.1 MB (10.53%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.py
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.py
@@ -6,9 +6,6 @@
 #         self.right = right
 class Solution:
     def maxPathSum(self, root: Optional[TreeNode]) -> int:
-        # if not root.left and not root.right:
-        #     return root.val
-
         def dfs(root):
             lhs = lpath = rhs = rpath = -10E6
 
@@ -24,3 +21,47 @@ class Solution:
 
         chs, _ = dfs(root)
         return chs
+
+
+### better name revised by chatgpt
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+from typing import Optional
+
+class Solution:
+    def maxPathSum(self, root: Optional[TreeNode]) -> int:
+        
+        def dfs(node):
+            # Base values for nodes without children
+            leftSum, leftEndPath = -float('inf'), -float('inf')
+            rightSum, rightEndPath = -float('inf'), -float('inf')
+
+            # Recurse if children are present
+            if node.left: 
+                leftSum, leftEndPath = dfs(node.left)
+            if node.right: 
+                rightSum, rightEndPath = dfs(node.right)
+            
+            # Compute the current highest sum
+            currentHighSum = max(node.val, 
+                                 leftSum, 
+                                 rightSum, 
+                                 leftEndPath + node.val, 
+                                 rightEndPath + node.val, 
+                                 leftEndPath + node.val + rightEndPath)
+            
+            # Compute the optimal end path for current node
+            optimalEndPath = node.val
+            if max(leftEndPath, rightEndPath) > 0:
+                optimalEndPath += max(leftEndPath, rightEndPath)
+            
+            return currentHighSum, optimalEndPath
+
+        max_sum, _ = dfs(root)
+        return max_sum
