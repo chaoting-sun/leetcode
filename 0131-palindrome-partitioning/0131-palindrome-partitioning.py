2c1b65299f365ba298e1b1a00210115ce82c7747
Sun, Chao-Ting <105558316+chaoting-sun@users.noreply.github.com>
2023-10-23 19:14:19 +0800

    Time: 541 ms (77.17%), Space: 30.7 MB (97.32%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.py
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -1,3 +1,17 @@
+### backtracking
+
+# time complexity: O(N*N^2)
+"""
+T(N) = T(N-1) + T(N-2) + ... + T(1)
+T(N-1) = T(N-2) + T(N-3) + ... + T(1)
+-
+-----------------------------------
+T(N) = 2*T(N-1) = O(2^N)
+
+it takes O(N) to check if a substring is a palindrome,
+so the time complexity is O(2^N) * O(N)
+"""
+
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
         ans = []
@@ -8,12 +22,15 @@ class Solution:
                 return
 
             for i in range(math.ceil(len(substr)/2.)):
+                # odd palindrome
                 delta = 0
                 while i-delta >= 0 and substr[i-delta] == substr[i+delta]:
                     delta += 1
 
                 if i - delta == -1:
                     backtrack(substr[i+delta:], part+[substr[:i+delta]], ans)
+
+                # even palindrome
 
                 if i + 1 >= len(substr):
                     break
@@ -27,3 +44,23 @@ class Solution:
 
         backtrack(s, [], ans)
         return ans
+
+
+### backtracking (cleaner)
+
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        ans = []
+        self.backtrack(s, [], ans)
+        return ans
+
+    def backtrack(self, substr, part, ans):
+        if not substr:
+            ans.append(part)
+            return
+        for i in range(len(substr)):
+            if self.isPalindrome(substr[:i+1]):
+                self.backtrack(substr[i+1:], part+[substr[:i+1]], ans)
+
+    def isPalindrome(self, substr):
+        return substr == substr[::-1]
