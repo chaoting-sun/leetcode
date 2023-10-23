d4a44b52282f43b2d5580a9e3d7ea80c8eb91c5d
Sun, Chao-Ting <105558316+chaoting-sun@users.noreply.github.com>
2023-10-23 22:00:00 +0800

    Time: 138 ms (23.22%), Space: 18.9 MB (58.73%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,7 +1,7 @@
 ### method1: sort
 
 # time complexity: O(nlogn)
-# space complexity: O(n)
+# space complexity: O(1)
 
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
@@ -17,6 +17,9 @@ class Solution:
 
 ### method2: hash table
 
+# time complexity: O(n)
+# space complexity: O(n)
+
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         st = set()
@@ -26,3 +29,18 @@ class Solution:
             else:
                 st.add(n)
         return st.pop()
+
+
+### method3: bit manipulation (XOR)
+
+# source: https://leetcode.com/problems/single-number/solutions/1771720/c-easy-solutions-sorting-xor-maps-or-frequency-array/?envType=list&envId=rr2ss0g5
+# time complexity: O(n)
+# space complexity: O(1)
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        ans = 0
+        for n in nums:
+            ans ^= n
+            print(ans)
+        return ans
