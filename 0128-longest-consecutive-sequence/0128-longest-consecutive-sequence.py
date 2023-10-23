477571f673c518185ca67159aa633e1a46a8ef80
Sun, Chao-Ting <105558316+chaoting-sun@users.noreply.github.com>
2023-10-23 16:42:42 +0800

    Time: 394 ms (55.69%), Space: 32.3 MB (21.06%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -1,3 +1,9 @@
+### method1: hash table
+
+# source: https://leetcode.com/problems/longest-consecutive-sequence/?envType=list&envId=rr2ss0g5
+# time complexity: O(N)
+# space complexity: O(N)
+
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
         res = 0
@@ -16,4 +22,33 @@ class Solution:
 
                 if mp[n] > res:
                     res = mp[n]
+        return res
+
+
+### method2: hash table
+
+# source: https://www.cnblogs.com/grandyang/p/4276225.html
+# time complexity: O(N)
+# space complexity: O(N)
+
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        res = 0
+        st = set(nums) # set
+
+        for n in nums:
+            if n not in st:
+                continue
+            
+            st.remove(n)
+
+            left = right = 1
+            while n - left in st:
+                st.remove(n-left)
+                left += 1
+            while n + right in st:
+                st.remove(n+right)
+                right += 1
+            
+            res = max(res, right+left-1)
         return res
