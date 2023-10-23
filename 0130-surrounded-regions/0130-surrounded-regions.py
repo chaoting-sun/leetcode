de173a886df15aa85f788b611af3364be57e5aa8
Sun, Chao-Ting <105558316+chaoting-sun@users.noreply.github.com>
2023-10-23 17:56:31 +0800

    Time: 200 ms (11.98%), Space: 34.2 MB (11.83%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -8,10 +8,10 @@ class Solution:
 
         q = deque()
 
-        for i in range(1, m-1):
+        for i in range(m):
             if board[i][0] == 'O': q.append((i, 0))
             if board[i][n-1] == 'O': q.append((i, n-1))
-        for j in range(1, n-1):
+        for j in range(n):
             if board[0][j] == 'O': q.append((0, j))
             if board[m-1][j] == 'O': q.append((m-1, j))
 
@@ -26,15 +26,11 @@ class Solution:
                 for dx, dy in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
                     newx = x + dx
                     newy = y + dy
-                    if (newx, newy) in [(0, 0), (m-1, 0), (0, n-1), (m-1, n-1)]:
-                        continue
                     if 0 <= newx < m and 0 <= newy < n and board[newx][newy] == 'O':
                         q.append((newx, newy))
-        
+
         for i in range(m):
             for j in range(n):
-                if (i, j) in [(0, 0), (m-1, 0), (0, n-1), (m-1, n-1)]:
-                    continue
                 if board[i][j] == 'O':
                     board[i][j] = 'X'
                 elif board[i][j] == '#':
