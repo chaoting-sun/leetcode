3e25addc7b4572a265fdb066c8be0e03a5dcb674
Sun, Chao-Ting <105558316+chaoting-sun@users.noreply.github.com>
2023-10-23 18:02:25 +0800

    Time: 134 ms (74.18%), Space: 18.2 MB (53.72%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -1,4 +1,8 @@
-import numpy
+### method1: bfs (multi-source bfs)
+
+# time complexity: O(mn)
+# space complexity: O(mn)
+
 class Solution:
     def solve(self, board: List[List[str]]) -> None:
         """
@@ -35,3 +39,32 @@ class Solution:
                     board[i][j] = 'X'
                 elif board[i][j] == '#':
                     board[i][j] = 'O'
+
+
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        m, n = len(board), len(board[0])
+
+        def dfs(i, j, m, n):
+            if not 0 <= i < m or not 0 <= j < n or board[i][j] != 'O':
+                return
+            board[i][j] = '#'
+            dfs(i+1, j, m, n)
+            dfs(i-1, j, m, n)
+            dfs(i, j+1, m, n)
+            dfs(i, j-1, m, n)
+
+        for i in range(m):
+            if board[i][0] == 'O': dfs(i, 0, m, n)
+            if board[i][n-1] == 'O': dfs(i, n-1, m, n)
+        for j in range(n):
+            if board[0][j] == 'O': dfs(0, j, m, n)
+            if board[m-1][j] == 'O': dfs(m-1, j, m, n)
+        
+        for i in range(m):
+            for j in range(n):
+                if board[i][j] == 'O': board[i][j] = 'X'
+                if board[i][j] == '#': board[i][j] = 'O'
