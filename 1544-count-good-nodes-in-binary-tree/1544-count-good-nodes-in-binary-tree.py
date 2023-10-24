c15e1b04db83197f1ed83dcc3894e396f2af57c3
Sun, Chao-Ting <105558316+chaoting-sun@users.noreply.github.com>
2023-10-24 16:59:24 +0800

    Time: 192 ms (53.21%), Space: 35 MB (78.44%) - LeetHub

--- 1544-count-good-nodes-in-binary-tree/1544-count-good-nodes-in-binary-tree.py
+++ 1544-count-good-nodes-in-binary-tree/1544-count-good-nodes-in-binary-tree.py
@@ -4,6 +4,12 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+### method1: dfs
+
+# time complexity: O(N)
+# space complexity: O(1)
+
 class Solution:
     def goodNodes(self, root: TreeNode) -> int:
         def dfs(node, maxValue):
@@ -19,3 +25,33 @@ class Solution:
             return nLeftGood + good + nRightGood
 
         return dfs(root, float('-inf'))
+
+
+### method2: bfs
+
+# time complexity: O(N)
+# space complexity: O(N)
+
+class Solution:
+    def goodNodes(self, root: TreeNode) -> int:
+        if not root:
+            return None
+        
+        nGoods = 0
+        q = deque([(root, float('-inf'))])
+
+        while q:
+            size = len(q)
+
+            for i in range(size):
+                node, maxValue = q.popleft()
+
+                maxValue = max(node.val, maxValue)
+
+                if node.val == maxValue:
+                    nGoods += 1
+
+                if node.left: q.append((node.left, maxValue))
+                if node.right: q.append((node.right, maxValue))
+        
+        return nGoods
