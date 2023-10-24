f87c2563f378b14e5925febf6204db9e99d6b5f8
Sun, Chao-Ting <105558316+chaoting-sun@users.noreply.github.com>
2023-10-24 15:29:22 +0800

    Time: 40 ms (75.06%), Space: 17.1 MB (97.58%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
@@ -7,6 +7,14 @@ class Node:
         self.random = random
 """
 
+### method: 2 pass (hash map)
+
+# source: https://leetcode.com/problems/copy-list-with-random-pointer/solutions/258935/detailed-explanation-with-pictures-c-javascript/?envType=list&envId=rr2ss0g5
+# method: copy the llist in the first traversal, and construct a hashmap from each original node to the corresponding new node. To add the random pointer, we first reach the random pointer from each node in the original llist and use the hashmap to get the new node.
+
+# time complexity: O(N)
+# space complexity: O(N)
+
 class Solution:
     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
         originToClone = {}
@@ -31,3 +39,38 @@ class Solution:
             curr = curr.next
         
         return cloneHead.next
+
+
+### optimization - make the space complexity O(1)
+
+# add new nodes in the original llist, in which each new node is at the right side of the cloned node. then, the random pointer of each new node is the next node of the random pointer of its previous node.
+
+# time complexity: O(N)
+# space complexity: O(1)
+
+class Solution:
+    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        if not head:
+            return None
+
+        curr = head
+
+        while curr:
+            cloneCurr = Node(curr.val, curr.next)
+            curr.next = cloneCurr
+            curr = cloneCurr.next
+        
+        curr = head
+        while curr:
+            if curr.random:
+                curr.next.random = curr.random.next
+            curr = curr.next.next
+        
+        cloneHead = head.next
+        cloneCurr = cloneHead
+
+        while cloneCurr.next:
+            cloneCurr.next = cloneCurr.next.next
+            cloneCurr = cloneCurr.next
+        
+        return cloneHead
