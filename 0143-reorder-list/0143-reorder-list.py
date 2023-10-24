75aa9fae8b85975728adb26f73afd2b97280a7a2
Sun, Chao-Ting <105558316+chaoting-sun@users.noreply.github.com>
2023-10-24 16:36:47 +0800

    Time: 86 ms (23.52%), Space: 26.3 MB (20.31%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.py
+++ 0143-reorder-list/0143-reorder-list.py
@@ -3,6 +3,12 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
+### method1: container + two pointers
+
+# time complexity: O(N)
+# space complexity: O(N)
+
 class Solution:
     def reorderList(self, head: Optional[ListNode]) -> None:
         """
@@ -24,3 +30,38 @@ class Solution:
                 container[j].next = container[i]
                 j -= 1
         container[j].next = None
+
+
+### method2: two pointers (fast and slow)
+
+# time complexity: O(N)
+# space complexity: O(1)
+
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        slow, fast = head, head
+
+        while fast.next and fast.next.next:
+            slow = slow.next
+            fast = fast.next.next
+        
+        prev, curr = None, slow.next
+        slow.next = None
+
+        while curr:
+            _next = curr.next
+            curr.next = prev
+            prev = curr
+            curr = _next
+
+        curr = head
+        secondCurr = prev
+
+        while curr and secondCurr:
+            curr_next, secondCurr_next = curr.next, secondCurr.next
+            curr.next = secondCurr
+            secondCurr.next = curr_next
+            curr, secondCurr = curr_next, secondCurr_next
