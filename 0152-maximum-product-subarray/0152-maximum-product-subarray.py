deb252149aa7c84d70be996d277b8c8ca3d2eb37
Sun, Chao-Ting <105558316+chaoting-sun@users.noreply.github.com>
2023-10-24 21:46:35 +0800

    Time: 70 ms (94.66%), Space: 16.6 MB (94.34%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.py
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.py
@@ -1,3 +1,8 @@
+### dp
+
+# time complexity: O(N)
+# space complexity: O(N)
+
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
         maxP = [0] * len(nums)
@@ -10,3 +15,22 @@ class Solution:
             minP[i] = min(maxP[i-1]*nums[i], minP[i-1]*nums[i], nums[i])
             res = max(res, maxP[i])
         return res
+
+
+### dp
+
+# time complexity: O(N)
+# space complexity: O(N)
+
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        global_max = local_min = local_max = nums[0]
+
+        for i in range(1, len(nums)):
+            if nums[i] < 0:
+                local_max, local_min = local_min, local_max
+            
+            local_max = max(local_max*nums[i], nums[i])
+            local_min = min(local_min*nums[i], nums[i])
+            global_max = max(local_max, global_max)
+        return global_max
