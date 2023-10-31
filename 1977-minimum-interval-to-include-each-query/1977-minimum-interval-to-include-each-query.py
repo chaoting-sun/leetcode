9daf3426dac51cd97799e5fc54fa92bef6195c9f
Sun, Chao-Ting <105558316+chaoting-sun@users.noreply.github.com>
2023-10-31 03:35:30 -0500

    Time: 1469 ms (33.94%), Space: 61.3 MB (19.72%) - LeetHub

--- 1977-minimum-interval-to-include-each-query/1977-minimum-interval-to-include-each-query.py
+++ 1977-minimum-interval-to-include-each-query/1977-minimum-interval-to-include-each-query.py
@@ -1,3 +1,11 @@
+### priority queue (min heap)
+
+# source: https://www.youtube.com/watch?v=5hQ5WWW5awQ
+# method: Sort both the queries and intervals so that we can handle queries from small to large values. Use a min heap to store the valid intervals for current query, which easily yield the smallest interval. The intervals in the min heap valid for the current query are left for the following queries as they may be valid for them. However, those invalid for the current query should be popped as they must not be valid for the following queries.
+
+# time complexity: O(NlogN+QlogQ) (N = len(intervals); Q = len(queries))
+# space complexity: O(N)
+
 class Solution:
     def minInterval(self, intervals: List[List[int]], queries: List[int]) -> List[int]:
         n = len(intervals)
@@ -23,3 +31,6 @@ class Solution:
             
         return ans
 
+
+### union find (hard to understand)
+# method: https://leetcode.com/problems/minimum-interval-to-include-each-query/solutions/1186840/python-union-find/?envType=list&envId=rr2ss0g5
