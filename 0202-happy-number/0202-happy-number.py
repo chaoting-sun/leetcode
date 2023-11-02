ef4691a02a0a91fa51f08e160a7842c2d2ed62dd
Sun, Chao-Ting <105558316+chaoting-sun@users.noreply.github.com>
2023-11-02 09:16:02 -0500

    Time: 46 ms (31.81%), Space: 16.1 MB (74.2%) - LeetHub

--- 0202-happy-number/0202-happy-number.py
+++ 0202-happy-number/0202-happy-number.py
@@ -1,9 +1,14 @@
+### method1: hash table
+
+# time complexity: ?
+# space complexity: O(N)
+
 class Solution:
     def isHappy(self, n: int) -> bool:
         squares = [i**2 for i in range(10)]
         st = set()
 
-        while n >= 4:
+        while n > 1:
             st.add(n)
 
             m = n
@@ -12,7 +17,34 @@ class Solution:
                 m, mod = divmod(m, 10)
                 cnt += squares[mod]
             n = cnt
-
             if cnt in st:
                 return False
         return n == 1
+
+
+### method1: fast and slow
+
+# time complexity: ?
+# space complexity: O(1)
+
+class Solution:
+    def isHappy(self, n: int) -> bool:
+
+        squares = [i**2 for i in range(10)]
+        def computeSum(n):
+            s = 0
+            while n > 0:
+                n, mod = divmod(n, 10)
+                s += squares[mod]
+            return s
+        
+        slow = fast = n
+
+        while True:
+            slow = computeSum(slow)
+            fast = computeSum(fast)
+            fast = computeSum(fast)
+            
+            if slow == 1 or slow == fast:
+                break
+        return slow == 1
