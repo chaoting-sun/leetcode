c3dbaaface601fc72e5951a19bf17fa4ab6c8f6b
Sun, Chao-Ting <105558316+chaoting-sun@users.noreply.github.com>
2023-11-09 08:16:49 -0600

    Time: 173 ms (15.83%), Space: 17.3 MB (77.34%) - LeetHub

---
+++ 1885-count-number-of-homogenous-substrings/1885-count-number-of-homogenous-substrings.py
@@ -0,0 +1,19 @@
+class Solution:
+    def countContiguous(self, n, cache):
+        if n in cache:
+            return cache[n]
+        cache[n] = (n+1)*n/2 % (10**9 + 7) 
+        return cache[n]
+
+    def countHomogenous(self, s: str) -> int:
+        left = right = 0
+        cache = {}
+        number = 0
+
+        while right < len(s):
+            while right < len(s) and s[left] == s[right]:
+                right += 1
+            number += self.countContiguous(right-left, cache)
+            left = right
+        
+        return int(number % (10**9 + 7))
