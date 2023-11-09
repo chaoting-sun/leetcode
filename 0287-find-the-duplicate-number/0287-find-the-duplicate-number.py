2a3f7855f576969b3b9c79bd7486819abcbcea87
Sun, Chao-Ting <105558316+chaoting-sun@users.noreply.github.com>
2023-11-09 03:00:05 -0600

    Time: 531 ms (57.39%), Space: 30.4 MB (94.53%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
@@ -10,3 +10,26 @@ class Solution:
             if nums[i+1]-nums[i] == 0:
                 return nums[i]
         return -1
+
+
+### method: Floyd's Cycle Detection
+
+# solution: https://www.youtube.com/watch?v=wjYnzkAhcNk
+# time complexity: O(N)
+# space complexity: O(1)
+
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        fast = slow = 0
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+            if slow == fast:
+                break
+        slow2 = 0
+        while True:
+            slow = nums[slow]
+            slow2 = nums[slow2]
+            if slow == slow2:
+                break
+        return slow
