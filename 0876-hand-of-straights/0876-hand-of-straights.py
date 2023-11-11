14e219a8ed844ba36228c5d7457789722d3ce53c
Sun, Chao-Ting <105558316+chaoting-sun@users.noreply.github.com>
2023-11-11 01:08:19 -0600

    Time: 160 ms (99.3%), Space: 18.2 MB (34.89%) - LeetHub

--- 0876-hand-of-straights/0876-hand-of-straights.py
+++ 0876-hand-of-straights/0876-hand-of-straights.py
@@ -1,3 +1,8 @@
+### method1: heap + hashtable
+
+# time complexity: O(NlogN)
+# space complexity: O(N)
+
 class Solution:
     def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
         if len(hand) < groupSize or len(hand) % groupSize != 0:
@@ -24,3 +29,28 @@ class Solution:
                     hashTable.pop(k)
         
         return len(hand) == 0
+
+
+class Solution:
+    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+        if len(hand) < groupSize or len(hand) % groupSize != 0:
+            return False
+
+        counts = {}
+        for h in hand:
+            counts[h] = counts.get(h, 0) + 1
+
+        sorted_keys = sorted(list(counts.keys()))
+
+        for k in sorted_keys:
+            if counts[k] == 0:
+                continue
+            
+            need = counts[k]
+            
+            for i in range(groupSize):
+                val = k + i
+                if val not in counts or counts[val] < need:
+                    return False
+                counts[val] -= need
+        return True
