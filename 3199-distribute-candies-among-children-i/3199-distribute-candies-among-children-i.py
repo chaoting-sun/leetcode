6096bb28a9e0fffc27d921f0ef374c747ab4abfb
Sun, Chao-Ting <105558316+chaoting-sun@users.noreply.github.com>
2023-11-11 10:30:34 -0600

    Time: 35 ms (100%), Space: 16.4 MB (50%) - LeetHub

--- 3199-distribute-candies-among-children-i/3199-distribute-candies-among-children-i.py
+++ 3199-distribute-candies-among-children-i/3199-distribute-candies-among-children-i.py
@@ -1,34 +1,5 @@
-# class Solution:
-#     def backtrack(self, iChild, nLeft, limit, cnt):
-#         if iChild == 4:
-#             if nLeft == 0:
-#                 return cnt + 1
-#             else:
-#                 return cnt
-        
-#         if iChild == 2:
-#             if nLeft <= limit:
-#                 return cnt + 1
-#             else:
-#                 return cnt
-        
-#         ub = min(nLeft, limit)
-        
-#         for nCandy in range(ub+1):
-#             if (nLeft-nCandy) / (2-iChild) > limit:
-#                 continue
-#             cnt = self.backtrack(iChild+1, nLeft-nCandy, limit, cnt)
-        
-#         return cnt        
-    
-    
-#     def distributeCandies(self, n: int, limit: int) -> int:
-#         if n / 3 > limit:
-#             return 0
-#         return self.backtrack(0, n, limit, 0)
-        
-        
-        
+### method: backtracking
+
 nDiff2Permut = { 1: 1, 2: 3, 3: 6 }
 
 class Solution:
