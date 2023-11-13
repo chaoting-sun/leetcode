75cb7312e8cff52643b34f14c5827b330e6d7696
Sun, Chao-Ting <105558316+chaoting-sun@users.noreply.github.com>
2023-11-13 01:04:12 -0600

    Time: 1902 ms (100%), Space: 16.2 MB (100%) - LeetHub

--- 3201-distribute-candies-among-children-ii/3201-distribute-candies-among-children-ii.py
+++ 3201-distribute-candies-among-children-ii/3201-distribute-candies-among-children-ii.py
@@ -1,3 +1,5 @@
+### method1
+
 class Solution:
     def distributeCandies(self, n: int, limit: int) -> int:
         firstMin = max(0, n-2*limit)
@@ -10,4 +12,25 @@ class Solution:
             secondMax = min(limit, n-i)
             ans += secondMax - secondMin + 1
 
+        return ans
+
+
+### method2
+
+class Solution:
+    def distributeCandies(self, n: int, limit: int) -> int:
+        if n > 3*limit:
+            return 0
+
+        ans = 0
+        for i in range(min(n, limit)+1):
+            nLeft = n-i
+            if nLeft > 2*limit:
+                continue
+            # By default, there are left+1 ways to split among 2nd and 3rd
+            # But 2*(left-limit) will be invalid
+            if nLeft <= limit:
+                ans += nLeft+1
+            else:
+                ans += nLeft+1-2*(nLeft-limit)
         return ans
