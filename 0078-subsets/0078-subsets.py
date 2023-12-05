92bcfe70b6335f009fe4c5afc41ad43e84fb451d
Sun, Chao-Ting <105558316+chaoting-sun@users.noreply.github.com>
2023-12-05 02:59:12 -0600

    Time: 33 ms (92.97%), Space: 16.6 MB (22.93%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,12 +1,47 @@
+### method1: backtracking
+
+# time complexity: O(2^n)
+# space complexity: O(2^n)
+
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         def backtrack(nums, i, ans, subset):
             if i >= len(nums):
                 ans.append(subset)
                 return
-            backtrack(nums, i+1, ans, subset)
-            backtrack(nums, i+1, ans, subset+[nums[i]])
+            backtrack(nums, i+1, ans, subset) # don't take
+            backtrack(nums, i+1, ans, subset+[nums[i]]) # take
 
         ans = []
         backtrack(nums, 0, ans, [])
+        return ans
+
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        def backtrack(nums, start, ans, subset):
+            ans.append(subset)
+            
+            for i in range(start, len(nums)):
+                backtrack(nums, i+1, ans, subset+[nums[i]])
+
+        ans = []
+        backtrack(nums, 0, ans, [])
+        return ans
+
+
+### method2: bit manipulation
+
+# time complexity: O(n*2^n)
+# space complexity: O(1)
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        ans = []
+        for i in range(1<<len(nums)):
+            subset = []
+            for j in range(len(nums)):
+                if (1<<j) & i:
+                    subset.append(nums[j])
+            ans.append(subset)
         return ans
