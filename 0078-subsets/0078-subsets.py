ddca256f28baf7dd70bfe90604b0d6f627867ab7
Sun, Chao-Ting <105558316+chaoting-sun@users.noreply.github.com>
2023-12-05 02:35:40 -0600

    Time: 42 ms (48.83%), Space: 16.6 MB (22.93%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,33 +1,12 @@
-# method1: backtracking; dfs
-
-# class Solution:
-#     def backtrack(self, nums, start, prevSub, subs):
-#         subs.append(prevSub)
-
-#         for i in range(start, len(nums)):
-#             currSub = prevSub + [nums[i]]
-#             self.backtrack(nums, i+1, currSub, subs)
-
-#     def subsets(self, nums: List[int]) -> List[List[int]]:
-#         subs = []
-
-#         self.backtrack(nums, 0, [], subs)
-#         return subs
-
-
-# method2: bit manipulation
-
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        subs = []
-        n = len(nums)
-        nComb = 1 << n
+        def backtrack(nums, i, ans, subset):
+            if i >= len(nums):
+                ans.append(subset)
+                return
+            backtrack(nums, i+1, ans, subset)
+            backtrack(nums, i+1, ans, subset+[nums[i]])
 
-        for s in range(nComb):
-            curr = []
-            for i in range(n):
-                if s & (1 << i):
-                    curr.append(nums[i])
-            subs.append(curr)
-
-        return subs
+        ans = []
+        backtrack(nums, 0, ans, [])
+        return ans
