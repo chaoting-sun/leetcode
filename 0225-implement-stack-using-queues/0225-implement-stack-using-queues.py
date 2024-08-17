ed28550687d1a3ec7b24163b6ded3a184f03ec25
Sun, Chao-Ting <chaoting0108@gmail.com>
2024-08-17 19:58:04 +0800

    Time: 42 ms (10.14%), Space: 16.7 MB (28.36%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.py
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.py
@@ -1,3 +1,14 @@
+"""
+Method 1: 2 queues
+
+Time:
+    push: O(1)
+    pop: O(n)
+    top: O(n)
+    empty: O(1)
+Space: O(n)
+"""
+
 class MyStack:
 
     def __init__(self):
@@ -8,7 +19,7 @@ class MyStack:
         if len(self.q1):
             self.q1.append(x)
         else:
-            self.q1.append(x)
+            self.q2.append(x)
 
     def pop(self) -> int:
         if len(self.q1):
@@ -36,7 +47,54 @@ class MyStack:
 
     def empty(self) -> bool:
         return not (len(self.q1) or len(self.q2))
-        
+
+"""
+Method 2: 1 queue
+
+Time:
+    push: O(1)
+    pop: O(n)
+    top: O(n)
+    empty: O(1)
+Space: O(n)
+"""
+
+# class MyStack:
+
+#     def __init__(self):
+#         self.q = deque()
+
+#     def push(self, x: int) -> None:
+
+#             self.q1.append(x)
+
+#     def pop(self) -> int:
+#         if len(self.q1):
+#             while len(self.q1) > 1:
+#                 self.q2.append(self.q1.popleft())
+#             return self.q1.popleft()
+#         else:
+#             while len(self.q2) > 1:
+#                 self.q1.append(self.q2.popleft())
+#             return self.q2.popleft()
+
+#     def top(self) -> int:
+#         if len(self.q1):
+#             while len(self.q1) > 1:
+#                 self.q2.append(self.q1.popleft())
+#             last = self.q1.pop()
+#             self.q2.append(last)
+#             return last
+#         else:
+#             while len(self.q2) > 1:
+#                 self.q1.append(self.q2.popleft())
+#             last = self.q2.pop()
+#             self.q1.append(last)
+#             return last
+
+#     def empty(self) -> bool:
+#         return not (len(self.q1) or len(self.q2))
+
 # Your MyStack object will be instantiated and called as such:
 # obj = MyStack()
 # obj.push(x)
