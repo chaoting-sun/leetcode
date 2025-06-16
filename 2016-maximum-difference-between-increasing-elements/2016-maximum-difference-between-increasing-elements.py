b10c6e771d49c09e79b0f95129aef531f1f03130
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-06-16 20:36:21 +0800

    Time: 0 ms (100%), Space: 17.8 MB (59.91%) - LeetHub

--- 2016-maximum-difference-between-increasing-elements/2016-maximum-difference-between-increasing-elements.py
+++ 2016-maximum-difference-between-increasing-elements/2016-maximum-difference-between-increasing-elements.py
@@ -8,14 +8,41 @@
 #                     max_difference = difference
 #         return max_difference
 
+# class Solution:
+#     def maximumDifference(self, nums: List[int]) -> int:
+#         max_difference = -1
+#         for divide_index in range(1, len(nums)):
+#             left_sorted = sorted(nums[0:divide_index])
+#             right_sorted = sorted(nums[divide_index: len(nums)])
+#             if len(left_sorted) > 0 and len(right_sorted) > 0:
+#                 difference = right_sorted[len(right_sorted)-1] - left_sorted[0]
+#                 if difference > 0 and difference > max_difference:
+#                     max_difference = difference
+#         return max_difference
+
 class Solution:
     def maximumDifference(self, nums: List[int]) -> int:
+        prefix_min, suffix_max = nums[:], nums[:]
+        
+        current_min = nums[0]
+        for i in range(len(nums)):
+            if nums[i] < current_min:
+                current_min = nums[i]
+                prefix_min[i] = nums[i]
+            else:
+                prefix_min[i] = current_min
+
+        current_max = nums[len(nums)-1]
+        for i in range(len(nums)-1, -1, -1):
+            if nums[i] > current_max:
+                current_max = nums[i]
+                suffix_max[i] = nums[i]
+            else:
+                suffix_max[i] = current_max
+
         max_difference = -1
-        for divide_index in range(1, len(nums)):
-            left_sorted = sorted(nums[0:divide_index])
-            right_sorted = sorted(nums[divide_index: len(nums)])
-            if len(left_sorted) > 0 and len(right_sorted) > 0:
-                difference = right_sorted[len(right_sorted)-1] - left_sorted[0]
-                if difference > 0 and difference > max_difference:
-                    max_difference = difference
+        for i in range(len(nums)):
+            difference = suffix_max[i] - prefix_min[i]
+            if difference > 0 and difference > max_difference:
+                max_difference = difference
         return max_difference
