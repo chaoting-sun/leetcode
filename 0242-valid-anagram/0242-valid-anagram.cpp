89d16db708ab1f96473f2f4d95d56b3766072547
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-11-25 12:38:16 +0800

    Time: 0 ms (100%), Space: 9.7 MB (47.57%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        vector<int> freq(26);
+        for (char ch: s) freq[ch - 'a']++;
+        for (char ch: t) freq[ch - 'a']--;
+        for (int i = 0; i < 26; i++) {
+            if (freq[i] != 0) return false;
+        }
+        return true;
+    }
+};
